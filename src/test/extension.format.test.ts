dc35f2fb344d3f43069594d23dce2e6d61d9a737
Don Jayamanne <don.jayamanne@yahoo.com>
2016-06-20 16:55:13 +1000

    Fixed linting unit test errors

--- src/test/extension.format.test.ts
+++ src/test/extension.format.test.ts
@@ -19,102 +19,102 @@ let pythonSettings = settings.PythonSettings.getInstance();
 let ch = vscode.window.createOutputChannel("Tests");
 let pythoFilesPath = path.join(__dirname, "..", "..", "src", "test", "pythonFiles", "formatting");
 
-function closeActiveEditor() {
-    if (vscode.window.activeTextEditor) {
-        vscode.commands.executeCommand("workbench.action.closeActiveEditor");
-    }
-}
 suite("Formatting", () => {
+    setup(() => {
+        return new Promise<any>(resolve => {
+            setTimeout(function () {
+                resolve();
+            }, 1000);
+        });
+    });
+    teardown(() => {
+        if (vscode.window.activeTextEditor) {
+            return vscode.commands.executeCommand("workbench.action.closeActiveEditor");
+        }
+        return Promise.resolve();
+    });
     test("AutoPep8", done => {
         let fileToFormat = path.join(pythoFilesPath, "beforeAutoPep8.py");
-        vscode.workspace.openTextDocument(fileToFormat).then(textDocument => {
-            vscode.window.showTextDocument(textDocument).then(textEditor => {
-                let formatter = new AutoPep8Formatter(ch, pythonSettings, pythoFilesPath);
-                return formatter.formatDocument(textDocument, null, null).then(edits => {
-                    textEditor.edit(editBuilder => {
-                        edits.forEach(edit => editBuilder.replace(edit.range, edit.newText));
-                    }).then(edited => {
-                        let formattedFile = path.join(pythoFilesPath, "afterAutoPep8.py");
-                        let formattedContents = fs.readFile(formattedFile, "utf-8", (error, data) => {
-                            if (error) {
-                                return assert.fail(error, "", "Failed to read formatted file");
-                            }
-                            assert.equal(textEditor.document.getText(), data, "Formatted text is not the same");
-                        });
-                    });
-                }, error => {
-                    assert.fail(error, "", "Error in Formatting, " + error);
-                });
-            }, error => {
-                assert.fail(error, "", "Error in Formatting, " + error);
+        let textEditor: vscode.TextEditor;
+        let textDocument: vscode.TextDocument;
+        vscode.workspace.openTextDocument(fileToFormat).then(document => {
+            textDocument = document;
+            return vscode.window.showTextDocument(textDocument);
+        }).then(editor => {
+            textEditor = editor;
+            let formatter = new AutoPep8Formatter(ch, pythonSettings, pythoFilesPath);
+            return formatter.formatDocument(textDocument, null, null);
+        }).then(edits => {
+            return textEditor.edit(editBuilder => {
+                edits.forEach(edit => editBuilder.replace(edit.range, edit.newText));
             });
-        }, error => {
-            assert.fail(error, "", "Error in Opening Document, " + error);
-        }).then(() => done(), done).then(() => closeActiveEditor(), closeActiveEditor);
+        }).then(edited => {
+            let formattedFile = path.join(pythoFilesPath, "afterAutoPep8.py");
+            let formattedContents = fs.readFile(formattedFile, "utf-8", (error, data) => {
+                if (error) {
+                    return assert.fail(error, "", "Failed to read formatted file");
+                }
+                assert.equal(textEditor.document.getText(), data, "Formatted text is not the same");
+            });
+        }).then(done, done);
     });
 
     test("Yapf", done => {
         let fileToFormat = path.join(pythoFilesPath, "beforeYapf.py");
-        vscode.workspace.openTextDocument(fileToFormat).then(textDocument => {
-            vscode.window.showTextDocument(textDocument).then(textEditor => {
-                let formatter = new YapfFormatter(ch, pythonSettings, pythoFilesPath);
-                return formatter.formatDocument(textDocument, null, null).then(edits => {
-                    textEditor.edit(editBuilder => {
-                        edits.forEach(edit => editBuilder.replace(edit.range, edit.newText));
-                    }).then(edited => {
-                        let formattedFile = path.join(pythoFilesPath, "afterYapf.py");
-                        let formattedContents = fs.readFile(formattedFile, "utf-8", (error, data) => {
-                            if (error) {
-                                return assert.fail(error, "", "Failed to read formatted file");
-                            }
-                            assert.equal(textEditor.document.getText(), data, "Formatted text is not the same");
-                        });
-                    });
-                }, error => {
-                    assert.fail(error, "", "Error in Formatting, " + error);
-                });
-            }, error => {
-                assert.fail(error, "", "Error in Formatting, " + error);
+        let textEditor: vscode.TextEditor;
+        let textDocument: vscode.TextDocument;
+        vscode.workspace.openTextDocument(fileToFormat).then(document => {
+            textDocument = document;
+            return vscode.window.showTextDocument(textDocument);
+        }).then(editor => {
+            textEditor = editor;
+            let formatter = new YapfFormatter(ch, pythonSettings, pythoFilesPath);
+            return formatter.formatDocument(textDocument, null, null);
+        }).then(edits => {
+            return textEditor.edit(editBuilder => {
+                edits.forEach(edit => editBuilder.replace(edit.range, edit.newText));
             });
-        }, error => {
-            assert.fail(error, "", "Error in Opening Document, " + error);
-        }).then(() => done(), done).then(() => closeActiveEditor(), closeActiveEditor);
+        }).then(edited => {
+            let formattedFile = path.join(pythoFilesPath, "afterYapf.py");
+            let formattedContents = fs.readFile(formattedFile, "utf-8", (error, data) => {
+                if (error) {
+                    return assert.fail(error, "", "Failed to read formatted file");
+                }
+                var x = textEditor.document.getText();
+                assert.equal(textEditor.document.getText(), data, "Formatted text is not the same");
+            });
+        }).then(done, done);
     });
 
     test("Yapf autoformat on save", done => {
         let formattedFile = path.join(pythoFilesPath, "afterYapfFormatOnSave.py");
-        let fileToFormat = path.join(pythoFilesPath, "beforeYapfFormatOnSave.py");
         let fileToCopyFrom = path.join(pythoFilesPath, "beforeYapfFormatOnSaveOriginal.py");
         let formattedFileContents = fs.readFileSync(formattedFile, "utf-8");
+
+        let fileToFormat = path.join(pythoFilesPath, "beforeYapf.py");
+        let textDocument: vscode.TextDocument;
+
         if (fs.existsSync(fileToFormat)) { fs.unlinkSync(fileToFormat); }
         fs.copySync(fileToCopyFrom, fileToFormat);
         const FORMAT_ON_SAVE = pythonSettings.formatting.formatOnSave;
         pythonSettings.formatting.formatOnSave = true;
         pythonSettings.formatting.provider = "yapf";
 
-        vscode.workspace.openTextDocument(fileToFormat).then(textDocument => {
-            return vscode.window.showTextDocument(textDocument).then(textEditor => {
-                return textEditor.edit(editBuilder => {
-                    editBuilder.insert(new vscode.Position(0, 0), "#");
-                }).then(edited => {
-                    return textDocument.save().then(saved => {
-                        return new Promise<any>((resolve, reject) => {
-                            setTimeout(() => {
-                                assert.equal(textDocument.getText(), formattedFileContents, "Formatted contents are not the same");
-                                resolve();
-                            }, 1000);
-                        });
-                    }, error => {
-                        assert.fail(error, "", "Error in Saving Document, " + error);
-                    });
-                }, error => {
-                    assert.fail(error, "", "Error in Editing Document, " + error);
-                });
-            }, error => {
-                assert.fail(error, "", "Error in Showing Document, " + error);
+        vscode.workspace.openTextDocument(fileToFormat).then(document => {
+            textDocument = document;
+            return vscode.window.showTextDocument(textDocument);
+        }).then(editor => {
+            return editor.edit(editBuilder => {
+                editBuilder.insert(new vscode.Position(0, 0), "#\n");
             });
-        }, error => {
-            assert.fail(error, "", "Error in Opening Document, " + error);
-        }).then(() => done(), done).then(() => closeActiveEditor(), closeActiveEditor);
+        }).then(saved => {
+            return new Promise<any>((resolve, reject) => {
+                setTimeout(() => {
+                    resolve();
+                }, 1000);
+            });
+        }).then(() => {
+            assert.equal(textDocument.getText(), formattedFileContents, "Formatted contents are not the same");
+        }).then(done, done);
     });
 });
