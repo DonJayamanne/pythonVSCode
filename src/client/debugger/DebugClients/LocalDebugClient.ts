cd99db81d254850aa550496ee25746ac416fd473
Don Jayamanne <don.jayamanne@yahoo.com>
2016-08-24 08:21:23 +1000

    Fixes #263

--- src/client/debugger/DebugClients/LocalDebugClient.ts
+++ src/client/debugger/DebugClients/LocalDebugClient.ts
@@ -122,6 +122,10 @@ export class LocalDebugClient extends DebugClient {
             this.pyProc.stderr.on("data", error => {
                 // We don't need to display the errors as stderr output is being captured by debugger
                 // and it gets sent out to the debug client
+                
+                // This is necessary so we read the stdout of the python process
+                // Else it just keep building up (related to issue #203 and #52)
+                let x = 0;
             });
             this.pyProc.stdout.on("data", d => {
                 // This is necessary so we read the stdout of the python process
