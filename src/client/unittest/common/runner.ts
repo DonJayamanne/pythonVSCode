850fe0d1029f77900d8eaf0a4ef8c6c758ed241f
Don Jayamanne <don.jayamanne@yahoo.com>
2016-08-28 11:44:05 +1000

    temp change, run tests in integrated terminal #239

--- src/client/unittest/common/runner.ts
+++ src/client/unittest/common/runner.ts
@@ -1,6 +1,18 @@
 import {execPythonFile} from './../../common/utils';
-import {CancellationToken, OutputChannel} from 'vscode';
+import {CancellationToken, OutputChannel, window} from 'vscode';
+import {getPythonInterpreterDirectory} from '../../common/utils';
 
+let terminal = null;
 export function run(file: string, args: string[], cwd: string, token?: CancellationToken, outChannel?: OutputChannel): Promise<string> {
+    // let cmd = '';
+    // getPythonInterpreterDirectory().then(pyPath => {
+    //     // Todo: Ability to run path using windows scripts (this works on mac - using bash)
+    //     cmd = `export PATH=$PATH:${pyPath};${file} ${args.join(' ')}`;
+    //     terminal = (<any>window).createTerminal(`Python Test Log`);
+    //     setTimeout(function() {
+    //         terminal.show();
+    //         terminal.sendText(cmd);
+    //     }, 1000);
+    // });
     return execPythonFile(file, args, cwd, true, (data: string) => outChannel.append(data), token);
 }
