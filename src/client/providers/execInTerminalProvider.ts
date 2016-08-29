1ff75c787e3efa706d33081cc8d7fca5092c8a12
James Booth <jabooth@gmail.com>
2016-08-29 11:01:52 +0100

    Add right-click explorer Exec in Terminal

--- src/client/providers/execInTerminalProvider.ts
+++ src/client/providers/execInTerminalProvider.ts
@@ -6,11 +6,12 @@ export function activateExecInTerminalProvider() {
     vscode.commands.registerCommand("python.execInTerminal", execInTerminal);
 }
 
-function execInTerminal(filePath: string) {
+function execInTerminal(fileUri?: vscode.Uri) {
     const currentPythonPath = settings.PythonSettings.getInstance().pythonPath;
+    let filePath: string;
 
-    console.log(filePath);
-    if (filePath === undefined) {
+    console.log(fileUri);
+    if (fileUri === undefined) {
         const activeEditor = vscode.window.activeTextEditor;
         if (activeEditor !== undefined) {
             if (!activeEditor.document.isUntitled) {
@@ -23,9 +24,11 @@ function execInTerminal(filePath: string) {
             vscode.window.showErrorMessage('No open file to run in terminal');
             return;
         }
+    } else {
+        filePath = fileUri.fsPath;
     }
 
-    const terminal = (<any>vscode.window).createTerminal(`Ext Terminal`);
+    const terminal = (<any>vscode.window).createTerminal(`Python`);
     // Temporary workaround until a promise of terminal readiness is available:
     // https://github.com/Tyriar/vscode-terminal-api-example/issues/2
     setTimeout(() => {
