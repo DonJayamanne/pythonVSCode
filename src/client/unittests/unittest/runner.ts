69d133b8a61cf2e9cb7da2345bc2129ca34c944d
Don Jayamanne <don.jayamanne@yahoo.com>
2016-09-02 12:47:59 +1000

    clear counters before tests #239

--- src/client/unittests/unittest/runner.ts
+++ src/client/unittests/unittest/runner.ts
@@ -17,10 +17,11 @@ interface TestStatusMap {
     status: TestStatus;
     summaryProperty: string;
 }
+
 const outcomeMapping = new Map<string, TestStatusMap>();
 outcomeMapping.set('passed', { status: TestStatus.Pass, summaryProperty: 'passed' });
-outcomeMapping.set('failed', { status: TestStatus.Fail, summaryProperty: 'failed' });
-outcomeMapping.set('error', { status: TestStatus.Error, summaryProperty: 'error' });
+outcomeMapping.set('failed', { status: TestStatus.Fail, summaryProperty: 'failures' });
+outcomeMapping.set('error', { status: TestStatus.Error, summaryProperty: 'errors' });
 outcomeMapping.set('skipped', { status: TestStatus.Skipped, summaryProperty: 'skipped' });
 
 interface ITestData {
@@ -29,14 +30,52 @@ interface ITestData {
     outcome: string;
     traceback: string;
 }
-const summary = {
-    passed: 0,
-    failed: 0,
-    error: 0,
-    skipped: 0
-};
 
 export function runTest(rootDirectory: string, tests: Tests, args: string[], testsToRun?: TestsToRun, token?: CancellationToken, outChannel?: OutputChannel): Promise<Tests> {
+    tests.summary.errors = 0;
+    tests.summary.failures = 0;
+    tests.summary.passed = 0;
+    tests.summary.skipped = 0;
+
+    const testLauncherFile = path.join(__dirname, '..', '..', '..', '..', 'pythonFiles', 'PythonTools', 'visualstudio_py_testlauncher.py');
+    const server = new Server();
+    server.on('error', (message: string, ...data: string[]) => {
+        console.log(`${message} ${data.join(' ')}`);
+    });
+    server.on('log', (message: string, ...data: string[]) => {
+    });
+    server.on('connect', (data) => {
+    });
+    server.on('start', (data: { test: string }) => {
+    });
+    server.on('result', (data: ITestData) => {
+        const test = tests.testFunctions.find(t => t.testFunction.nameToRun === data.test);
+        if (test) {
+            const statusDetails = outcomeMapping.get(data.outcome);
+            test.testFunction.status = statusDetails.status;
+            test.testFunction.message = data.message;
+            test.testFunction.traceback = data.traceback;
+            tests.summary[statusDetails.summaryProperty] += 1;
+        }
+    });
+    server.on('socket.disconnected', (data) => {
+    });
+
+    return server.start().then(port => {
+        let testPaths: string[] = getIdsOfTestsToRun(tests, testsToRun);
+        for (let counter = 0; counter < testPaths.length; counter++) {
+            testPaths[counter] = '-t' + testPaths[counter].trim();
+        }
+        let testArgs = buildTestArgs(args);
+        testArgs = [testLauncherFile].concat(testArgs).concat(`--result-port=${port}`).concat(testPaths);
+        return run(settings.pythonPath, testArgs, rootDirectory, token, outChannel);
+    }).then(() => {
+        updateResults(tests);
+        return tests;
+    });
+}
+
+function buildTestArgs(args: string[]): string[] {
     let startDirectory = '.';
     let pattern = 'test*.py';
     const indexOfStartDir = args.findIndex(arg => arg.indexOf('-s') === 0);
@@ -67,74 +106,34 @@ export function runTest(rootDirectory: string, tests: Tests, args: string[], tes
             }
         }
     }
+    const failFast = args.some(arg => arg.trim() === '-f' || arg.trim() === '--failfast');
     const verbosity = args.some(arg => arg.trim().indexOf('-v') === 0) ? 2 : 1;
-    args = [`--us=${startDirectory}`, `--up=${pattern}`, `--uvInt=${verbosity}`];
-
-    summary.error = 0;
-    summary.failed = 0;
-    summary.skipped = 0;
-    summary.passed = 0;
-    let testPaths: string[] = [];
+    const testArgs = [`--us=${startDirectory}`, `--up=${pattern}`, `--uvInt=${verbosity}`];
+    if (failFast) {
+        testArgs.push('--uf');
+    }
+    return testArgs;
+}
+function getIdsOfTestsToRun(tests: Tests, testsToRun: TestsToRun): string[] {
+    const testIds = [];
     if (testsToRun && testsToRun.testFolder) {
         // Get test ids of files in these folders
         testsToRun.testFolder.map(folder => {
             tests.testFiles.forEach(f => {
                 if (f.fullPath.startsWith(folder.name)) {
-                    testPaths.push(f.nameToRun);
+                    testIds.push(f.nameToRun);
                 }
             });
         });
     }
     if (testsToRun && testsToRun.testFile) {
-        testPaths = testPaths.concat(testsToRun.testFile.map(f => f.nameToRun));
+        testIds.push(...testsToRun.testFile.map(f => f.nameToRun));
     }
     if (testsToRun && testsToRun.testSuite) {
-        testPaths = testPaths.concat(testsToRun.testSuite.map(f => f.nameToRun));
+        testIds.push(...testsToRun.testSuite.map(f => f.nameToRun));
     }
     if (testsToRun && testsToRun.testFunction) {
-        testPaths = testPaths.concat(testsToRun.testFunction.map(f => f.nameToRun));
+        testIds.push(...testsToRun.testFunction.map(f => f.nameToRun));
     }
-    for (let counter = 0; counter < testPaths.length; counter++) {
-        testPaths[counter] = '-t' + testPaths[counter].trim();
-    }
-    const testLauncherFile = path.join(__dirname, '..', '..', '..', '..', 'pythonFiles', 'PythonTools', 'visualstudio_py_testlauncher.py');
-    const server = new Server();
-    server.on('error', (message: string, ...data: string[]) => {
-        console.log(`${message} ${data.join(' ')}`);
-    });
-    server.on('log', (message: string, ...data: string[]) => {
-    });
-    server.on('connect', (data) => {
-    });
-    server.on('start', (data: { test: string }) => {
-        if (!data || typeof data.test !== 'string' || data.test.length === 0) {
-            return;
-        }
-        const testFn = data.test.substring(data.test.lastIndexOf('.') + 1);
-        const testClass = data.test.substring(0, data.test.lastIndexOf('.'));
-    });
-    server.on('result', (data: ITestData) => {
-        const test = tests.testFunctions.find(t => t.testFunction.nameToRun === data.test);
-        const statusDetails = outcomeMapping.get(data.outcome);
-        if (test) {
-            let statusToLog = '';
-            test.testFunction.status = statusDetails.status;
-            test.testFunction.message = data.message;
-            test.testFunction.traceback = data.traceback;
-            summary[statusDetails.summaryProperty] += 1;
-        }
-    });
-    server.on('socket.disconnected', (data) => {
-    });
-
-    return server.start().then(port => {
-        return run(settings.pythonPath, [testLauncherFile].concat(args).concat(`--result-port=${port}`).concat(testPaths), rootDirectory, token, outChannel);
-    }).then(() => {
-        tests.summary.errors = summary.error;
-        tests.summary.failures = summary.failed;
-        tests.summary.passed = summary.passed;
-        tests.summary.skipped = summary.skipped;
-        updateResults(tests);
-        return tests;
-    });
+    return testIds;
 }
