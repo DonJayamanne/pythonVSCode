178a24b104cbbe04049c3c8a11239bccb10b0d55
Don Jayamanne <don.jayamanne@yahoo.com>
2016-09-08 15:19:13 +1000

    search known paths

--- src/client/providers/setInterpreterProvider.ts
+++ src/client/providers/setInterpreterProvider.ts
@@ -7,13 +7,45 @@ import * as utils from "./../common/utils";
 let ncp = require("copy-paste");
 
 // where to find the Python binary within a conda env
-const CONDA_RELATIVE_PY_PATH = utils.IS_WINDOWS ? ['python'] : ['bin', 'python'] 
-const REPLACE_PYTHONPATH_REGEXP = /("python\.pythonPath"\s*:\s*)"(.*)"/;
+const CONDA_RELATIVE_PY_PATH = utils.IS_WINDOWS ? ['python'] : ['bin', 'python']
+const REPLACE_PYTHONPATH_REGEXP = /("python\.pythonPath"\s*:\s*)"(.*)"/g;
 
 interface PythonPathSuggestion {
     label: string, // myenvname
     path: string,  // /full/path/to/bin/python
     type: string   // conda
+}
+
+function getSearchPaths(): string[] {
+    if (utils.IS_WINDOWS) {
+        return [
+            'C:\\Python2.7',
+            'C:\\Python27',
+            'C:\\Python3.4',
+            'C:\\Python34',
+            'C:\\Python3.5',
+            'C:\\Python35',
+            'C:\\Python35-32',
+            'C:\\Program Files (x86)\\Python 2.7',
+            'C:\\Program Files (x86)\\Python 3.4',
+            'C:\\Program Files (x86)\\Python 3.5',
+            'C:\\Program Files (x64)\\Python 2.7',
+            'C:\\Program Files (x64)\\Python 3.4',
+            'C:\\Program Files (x64)\\Python 3.5',
+            'C:\\Program Files\\Python 2.7',
+            'C:\\Program Files\\Python 3.4',
+            'C:\\Program Files\\Python 3.5'
+        ].map(p => path.join(p, 'python.exe'));
+    } else {
+        const pyPaths = [];
+        const pyExecutables = ['python', 'python2.6', 'python2.7', 'python3.3', 'python3.4', 'python3.5'];
+        ['/usr/local/bin', '/usr/bin', '/bin', '/usr/sbin', '/sbin'].forEach(p => {
+            pyExecutables.forEach(pyExecName => {
+                pyPaths.push(path.join(p, pyExecName));
+            });
+        });
+        return pyPaths;
+    }
 }
 
 function workspaceSettingsPath() {
@@ -40,6 +72,19 @@ export function activateSetInterpreterProvider() {
     vscode.commands.registerCommand("python.setInterpreter", setInterpreter);
 }
 
+function suggestionsFromKnownPaths(): Promise<PythonPathSuggestion[]> {
+    return new Promise(resolve => {
+        const validPaths = getSearchPaths().map(p => utils.validatePath(p));
+        Promise.all<string>(validPaths).then(paths => {
+            const suggestions = paths.filter(p => p.length > 0).map(p => {
+                return <PythonPathSuggestion>{
+                    label: path.basename(p), path: p, type: ''
+                }
+            });
+            resolve(suggestions);
+        });
+    });
+}
 function suggestionsFromConda(): Promise<PythonPathSuggestion[]> {
     return new Promise((resolve, reject) => {
         // interrogate conda (if it's on the path) to find all environments
@@ -71,7 +116,7 @@ function suggestionsFromConda(): Promise<PythonPathSuggestion[]> {
     });
 }
 
-function suggestionToQuickPickItem(suggestion: PythonPathSuggestion) : vscode.QuickPickItem {
+function suggestionToQuickPickItem(suggestion: PythonPathSuggestion): vscode.QuickPickItem {
     return {
         label: suggestion.label,
         description: suggestion.type,
@@ -80,25 +125,43 @@ function suggestionToQuickPickItem(suggestion: PythonPathSuggestion) : vscode.Qu
 }
 
 function suggestPythonPaths(): Promise<vscode.QuickPickItem[]> {
-
     // For now we only interrogate conda for suggestions.
     const condaSuggestions = suggestionsFromConda();
+    const knownPathSuggestions = suggestionsFromKnownPaths();
 
     // Here we could also look for virtualenvs/default install locations...
 
-    return condaSuggestions.then(
-        suggestions => suggestions.map(suggestionToQuickPickItem)
-    );
+    return Promise.all<PythonPathSuggestion[]>([condaSuggestions, knownPathSuggestions]).then(suggestions => {
+        const quickPicks: vscode.QuickPickItem[] = [];
+        suggestions.forEach(list => {
+            quickPicks.push(...list.map(suggestionToQuickPickItem));
+        });
+
+        return quickPicks;
+    });
 }
 
-function setPythonPath(pythonPath: string) {
-    vscode.workspace.openTextDocument(workspaceSettingsPath())
+function setPythonPath(pythonPath: string, created: boolean = false) {
+    const settingsFile = workspaceSettingsPath();
+    utils.validatePath(settingsFile)
+        .then(validatedPath => {
+            if (validatedPath.length === 0 && created === true) {
+                // Something went wrong
+                return Promise.reject<any>('Unable to create/open the Workspace Settings file');
+            }
+            if (validatedPath.length === 0 && !created) {
+                return new Promise<any>((resolve, reject) => {
+                    vscode.commands.executeCommand('workbench.action.openWorkspaceSettings').then(() => resolve(null), reject);
+                });
+            }
+            return vscode.workspace.openTextDocument(settingsFile)
+        })
         .then(doc => {
-            const settingsText = doc.getText();
+            const settingsText = doc ? doc.getText() : '';
             if (settingsText.search(REPLACE_PYTHONPATH_REGEXP) === -1) {
                 // Can't find the setting to replace - will just have to offer a copy button and instruct them to edit themselves.
                 openWorkspaceSettings().then(() => {
-                    const copyMsg =  "Copy to Clipboard"
+                    const copyMsg = "Copy to Clipboard"
                     const newEntry = `"python.pythonPath": "${pythonPath}"`;
                     vscode.window.showInformationMessage(`Please add an entry: ${newEntry}`, copyMsg)
                         .then(item => {
@@ -112,7 +175,7 @@ function setPythonPath(pythonPath: string) {
                 const newSettingsText = settingsText.replace(REPLACE_PYTHONPATH_REGEXP, `$1"${pythonPath}"`);
                 replaceContentsOfFile(doc, newSettingsText).then(
                     () => {
-                        vscode.window.setStatusBarMessage(`Workspace Interpreter set to ${pythonPath}`);
+                        vscode.window.setStatusBarMessage(`Workspace Interpreter set to ${pythonPath}`, 1000);
                         // As the file is saved the following should be the same as each other but they
                         // aren't - some form of race condition?
                         // const currentPythonPath = settings.PythonSettings.getInstance().pythonPath;
@@ -121,6 +184,8 @@ function setPythonPath(pythonPath: string) {
                     }
                 )
             }
+        }).catch(reason => {
+            vscode.window.showErrorMessage('Failed to set the interpreter. ' + reason);
         });
 }
 
@@ -132,12 +197,14 @@ function presentQuickPickOfSuggestedPythonPaths() {
         placeHolder: `current: ${currentPythonPath}`
     }
 
-    vscode.window.showQuickPick(suggestPythonPaths(), quickPickOptions).then(
-        value => {
-            if (value !== undefined) {
-                setPythonPath(value.detail);
-            }
-        })
+    suggestPythonPaths().then(suggestions => {
+        vscode.window.showQuickPick(suggestions, quickPickOptions).then(
+            value => {
+                if (value !== undefined) {
+                    setPythonPath(value.detail);
+                }
+            });
+    });
 }
 
 function setInterpreter() {
@@ -151,12 +218,5 @@ function setInterpreter() {
         vscode.window.showErrorMessage("The interpreter can only be set within a workspace (open a folder)")
         return
     }
-    vscode.workspace.openTextDocument(settingsPath).then(
-        presentQuickPickOfSuggestedPythonPaths,
-        () => {
-            // No settings present yet! Trigger the opening of the workspace settings for the first time
-            // then present the picker.
-            openWorkspaceSettings().then(presentQuickPickOfSuggestedPythonPaths)
-        }
-    )
+    presentQuickPickOfSuggestedPythonPaths();
 }
