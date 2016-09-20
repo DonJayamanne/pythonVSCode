4761736611357b6350071c6a3ff4220dfe38913a
Don Jayamanne <don.jayamanne@yahoo.com>
2016-09-20 16:06:44 +1000

    support latex and __dirname in bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,18 @@
 var path = require('path');
 var browserPath = path.join(__dirname, 'src', 'client', 'jupyter', 'browser');
 module.exports = {
-    //context: browserPath,
+    context: browserPath,
+    node: {
+        __filename: false,
+        // If this is not used then __dirname in bundled scripts is set to / and mathajax-electron
+        // https://github.com/nteract/mathjax-electron/blob/master/src/mathjax-electron.js#L22
+        // script.src = path.join(__dirname, "..", "resources", "MathJax", "MathJax.js?delayStartupUntil=configured");
+        __dirname: false
+    },
     entry: path.join(browserPath, 'main.ts'),
     output: {
-        filename: path.join(__dirname, 'out', 'client', 'jupyter', 'browser', 'bundle.js')
+        filename: path.join(__dirname, 'out', 'client', 'jupyter', 'browser', 'bundle.js'),
+        publicPath: browserPath
     },
     resolve: {
         root: browserPath,
