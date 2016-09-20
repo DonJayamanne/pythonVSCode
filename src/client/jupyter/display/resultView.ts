d968608e4a2ce8a4b50910206b4b3189597a99d5
Don Jayamanne <don.jayamanne@yahoo.com>
2016-09-20 16:07:59 +1000

    temporary hacks for latex

--- src/client/jupyter/display/resultView.ts
+++ src/client/jupyter/display/resultView.ts
@@ -3,6 +3,7 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as helpers from '../../common/helpers';
 
 export class TextDocumentContentProvider implements vscode.TextDocumentContentProvider {
     private _onDidChange = new vscode.EventEmitter<vscode.Uri>();
@@ -11,7 +12,7 @@ export class TextDocumentContentProvider implements vscode.TextDocumentContentPr
     private results: any[];
     public provideTextDocumentContent(uri: vscode.Uri, token: vscode.CancellationToken): Thenable<string> {
         this.lastUri = uri;
-        return Promise.resolve(this.generateResultsView());
+        return this.generateResultsView();
     }
 
     get onDidChange(): vscode.Event<vscode.Uri> {
@@ -40,7 +41,16 @@ export class TextDocumentContentProvider implements vscode.TextDocumentContentPr
         return `<head></head><body>${error}</body>`;
     }
 
-    private generateResultsView(): string {
+    private tmpHtmlFile: string;
+    private generateResultsView(): Promise<string> {
+        // Use this hack only when using the iframe option
+        const style = `
+                <style type="text/css">
+                html, body{
+                    height:100%;
+                    width:100%;
+                }
+                </style>`;
         const html = `
                 <head>
                 </head>
@@ -48,10 +58,49 @@ export class TextDocumentContentProvider implements vscode.TextDocumentContentPr
                     <script type="text/javascript">
                         window.JUPYTER_DATA = ${JSON.stringify(this.results)};
                     </script>
-                    <script src="${this.getScriptFilePath('bundle.js')}"></script>
+                    <script src="${this.getScriptFilePath('bundle.js')}?x=${new Date().getMilliseconds()}"></script>
                 </body>
             `;
-        fs.writeFileSync('/Users/donjayamanne/.vscode/extensions/pythonVSCode/results.html', html);
-        return html;
+        // fs.writeFileSync('/Users/donjayamanne/.vscode/extensions/pythonVSCode/results.html', html);
+        return Promise.resolve(html);
+
+        // let htmlFile: helpers.Deferred<string> = helpers.createDeferred<string>();
+        // if (this.tmpHtmlFile) {
+        //     fs.exists(this.tmpHtmlFile, exists => {
+        //         if (exists) {
+        //             return htmlFile.resolve(this.tmpHtmlFile);
+        //         }
+        //         helpers.createTemporaryFile('.html').then(tmpFile => {
+        //             htmlFile.resolve(tmpFile.filePath);
+        //         });
+        //     });
+        // }
+        // else {
+        //     helpers.createTemporaryFile('.html').then(tmpFile => {
+        //         htmlFile.resolve(tmpFile.filePath);
+        //     });
+        // }
+
+        // return htmlFile.promise.then(htmlFileName => {
+        //     const htmlContent = `
+        //     <head>
+        //         <style type="text/css">
+        //         html, body{
+        //             height:100%;
+        //             width:100%;
+        //         }
+        //         </style>
+        //     </head>
+        //     <body><iframe frameborder="0" style="border: 0px solid white;height:100%;width:100%;" src="${htmlFileName}" seamless></iframe></body>`;
+        //     return new Promise<string>((resolve, reject) => {
+        //         fs.writeFile(htmlFileName, html, err => {
+        //             if (err) {
+        //                 return reject(err);
+        //             }
+        //             fs.writeFileSync('/Users/donjayamanne/.vscode/extensions/pythonVSCode/results.html', htmlContent);
+        //             resolve(htmlContent);
+        //         });
+        //     });
+        // });
     }
 }
