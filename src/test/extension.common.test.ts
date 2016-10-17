aff2f2e455503e096e85ba50a583ba523dfc8c02
Don Jayamanne <don.jayamanne@yahoo.com>
2016-10-17 17:11:07 +1100

    enabled tests

--- src/test/extension.common.test.ts
+++ src/test/extension.common.test.ts
@@ -1,35 +1,35 @@
-// //
-// // Note: This example test is leveraging the Mocha test framework.
-// // Please refer to their documentation on https://mochajs.org/ for help.
-// //
+//
+// Note: This example test is leveraging the Mocha test framework.
+// Please refer to their documentation on https://mochajs.org/ for help.
+//
 
-// // Place this right on top
-// import { initialize } from './initialize';
-// // The module 'assert' provides assertion methods from node
-// import * as assert from 'assert';
+// Place this right on top
+import { initialize } from './initialize';
+// The module 'assert' provides assertion methods from node
+import * as assert from 'assert';
 
-// // You can import and use all API from the 'vscode' module
-// // as well as import your extension to test it
-// import * as vscode from 'vscode';
-// import { execPythonFile } from '../client/common/utils';
+// You can import and use all API from the 'vscode' module
+// as well as import your extension to test it
+import * as vscode from 'vscode';
+import { execPythonFile } from '../client/common/utils';
 
-// // Defines a Mocha test suite to group tests of similar kind together
-// suite('ChildProc', () => {
-//     setup(done => {
-//         initialize().then(() => done(), done);
-//     });
-//     test('Standard Response', done => {
-//         execPythonFile('python', ['-c', 'print(1)'], __dirname, false).then(data => {
-//             assert.ok(data === '1\n');
-//         }).then(done, done);
-//     });
-//     test('Error Response', done => {
-//         execPythonFile('python', ['-c', 'print(1'], __dirname, false).then(data => {
-//             assert.ok(false);
-//             done();
-//         }).catch(() => {
-//             assert.ok(true);
-//             done();
-//         });
-//     });
-// });
+// Defines a Mocha test suite to group tests of similar kind together
+suite('ChildProc', () => {
+    setup(done => {
+        initialize().then(() => done(), done);
+    });
+    test('Standard Response', done => {
+        execPythonFile('python', ['-c', 'print(1)'], __dirname, false).then(data => {
+            assert.ok(data === '1\n');
+        }).then(done, done);
+    });
+    test('Error Response', done => {
+        execPythonFile('python', ['-c', 'print(1'], __dirname, false).then(data => {
+            assert.ok(false);
+            done();
+        }).catch(() => {
+            assert.ok(true);
+            done();
+        });
+    });
+});
