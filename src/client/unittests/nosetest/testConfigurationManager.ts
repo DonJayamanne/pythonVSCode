9228965dfa44effad39d89df05dc736ea0e43caf
Don Jayamanne <don.jayamanne@yahoo.com>
2016-11-06 13:46:13 +1100

    do not prompt if config file exists

--- src/client/unittests/nosetest/testConfigurationManager.ts
+++ src/client/unittests/nosetest/testConfigurationManager.ts
@@ -3,7 +3,7 @@ import { TestConfigurationManager } from '../common/testConfigurationManager';
 import * as fs from 'fs';
 import * as path from 'path';
 import { Installer, Product } from '../../common/installer';
- 
+
 export class ConfigurationManager extends TestConfigurationManager {
     public enable(): Thenable<any> {
         const pythonConfig = vscode.workspace.getConfiguration('python');
@@ -14,42 +14,36 @@ export class ConfigurationManager extends TestConfigurationManager {
         return pythonConfig.update('unitTest.nosetestsEnabled', false);
     }
 
-    private static configFilesExist(rootDir: string): Promise<boolean> {
-        const promises = [
-            new Promise<boolean>(resolve => {
-                fs.exists(path.join(rootDir, '.noserc'), exists => { resolve(true); });
-            }),
-            new Promise<boolean>(resolve => {
-                fs.exists(path.join(rootDir, 'nose.cfg'), exists => { resolve(true); });
-            })];
+    private static configFilesExist(rootDir: string): Promise<string[]> {
+        const promises = ['.noserc', 'nose.cfg'].map(cfg => {
+            return new Promise<string>(resolve => {
+                fs.exists(path.join(rootDir, cfg), exists => { resolve(exists ? cfg : ''); });
+            });
+        });
         return Promise.all(promises).then(values => {
-            return values.some(exists => exists);
+            return values.filter(exists => exists.length > 0);
         });
     }
     public configure(rootDir: string): Promise<any> {
         const args = [];
         const configFileOptionLabel = 'Use existing config file';
-        const options: vscode.QuickPickItem[] = [];
         let installer = new Installer(this.outputChannel);
-        return ConfigurationManager.configFilesExist(rootDir).then(configExists => {
-            if (configExists) {
-                options.push({
-                    label: configFileOptionLabel,
-                    description: '.noserc or nose.cfg'
-                });
+        return ConfigurationManager.configFilesExist(rootDir).then(configFiles => {
+            if (configFiles.length > 0) {
+                return Promise.resolve();
             }
-        }).then(() => {
-            return this.getTestDirs(rootDir);
-        }).then(subDirs => {
-            return this.selectTestDir(rootDir, subDirs, options);
-        }).then(testDir => {
-            if (typeof testDir === 'string' && testDir !== configFileOptionLabel) {
-                args.push(testDir);
-            }
+
+            return this.getTestDirs(rootDir).then(subDirs => {
+                return this.selectTestDir(rootDir, subDirs);
+            }).then(testDir => {
+                if (typeof testDir === 'string' && testDir !== configFileOptionLabel) {
+                    args.push(testDir);
+                }
+            });
         }).then(() => {
             return installer.isProductInstalled(Product.nosetest);
         }).then(installed => {
-            if (!installed){
+            if (!installed) {
                 return installer.installProduct(Product.nosetest);
             }
         }).then(() => {
--- src/client/unittests/pytest/testConfigurationManager.ts
+++ src/client/unittests/pytest/testConfigurationManager.ts
@@ -14,19 +14,14 @@ export class ConfigurationManager extends TestConfigurationManager {
         return pythonConfig.update('unitTest.pyTestEnabled', false);
     }
 
-    private static configFilesExist(rootDir: string): Promise<boolean> {
-        const promises = [
-            new Promise<boolean>(resolve => {
-                fs.exists(path.join(rootDir, 'pytest.ini'), exists => { resolve(true); });
-            }),
-            new Promise<boolean>(resolve => {
-                fs.exists(path.join(rootDir, 'tox.ini'), exists => { resolve(true); });
-            }),
-            new Promise<boolean>(resolve => {
-                fs.exists(path.join(rootDir, 'setup.cfg'), exists => { resolve(true); });
-            })];
+    private static configFilesExist(rootDir: string): Promise<string[]> {
+        const promises = ['pytest.ini', 'tox.ini', 'setup.cfg'].map(cfg => {
+            return new Promise<string>(resolve => {
+                fs.exists(path.join(rootDir, cfg), exists => { resolve(exists ? cfg : ''); });
+            });
+        });
         return Promise.all(promises).then(values => {
-            return values.some(exists => exists);
+            return values.filter(exists => exists.length > 0);
         });
     }
     public configure(rootDir: string): Promise<any> {
@@ -34,25 +29,28 @@ export class ConfigurationManager extends TestConfigurationManager {
         const configFileOptionLabel = 'Use existing config file';
         const options: vscode.QuickPickItem[] = [];
         let installer = new Installer(this.outputChannel);
-        return ConfigurationManager.configFilesExist(rootDir).then(configExists => {
-            if (configExists) {
+        return ConfigurationManager.configFilesExist(rootDir).then(configFiles => {
+            if (configFiles.length > 0 && configFiles.length !== 1 && configFiles[0] !== 'setup.cfg') {
+                return Promise.resolve();
+            }
+
+            if (configFiles.length === 1 && configFiles[0] === 'setup.cfg') {
                 options.push({
                     label: configFileOptionLabel,
-                    description: 'pytest.ini, tox.ini or setup.cfg'
+                    description: 'setup.cfg'
                 });
             }
-        }).then(() => {
-            return this.getTestDirs(rootDir);
-        }).then(subDirs => {
-            return this.selectTestDir(rootDir, subDirs, options);
-        }).then(testDir => {
-            if (typeof testDir === 'string' && testDir !== configFileOptionLabel) {
-                args.push(testDir);
-            }
+            return this.getTestDirs(rootDir).then(subDirs => {
+                return this.selectTestDir(rootDir, subDirs, options);
+            }).then(testDir => {
+                if (typeof testDir === 'string' && testDir !== configFileOptionLabel) {
+                    args.push(testDir);
+                }
+            });
         }).then(() => {
             return installer.isProductInstalled(Product.pytest);
         }).then(installed => {
-            if (!installed){
+            if (!installed) {
                 return installer.installProduct(Product.pytest);
             }
         }).then(() => {
