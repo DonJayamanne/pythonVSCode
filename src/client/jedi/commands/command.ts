1a4be4f0689161e2021314973bc4b7a826c30fda
Don Jayamanne <don.jayamanne@yahoo.com>
2017-05-08 21:47:19 +1000

    completion parser

--- src/client/jedi/commands/command.ts
+++
@@ -1,7 +0,0 @@
-
-import { SocketStream } from "../../common/comms/SocketStream";
-
-export abstract class Command {
-    abstract writeToStream(stream: SocketStream);
-    abstract parseResponse<T>(data: Object): T;
-}
---
+++ src/client/jedi/parsers/CompletionParser.ts
@@ -0,0 +1,6 @@
+import { CompletionItem } from 'vscode';
+export class CompletionParser {
+    public static parse(data: Object): CompletionItem[] {
+        return [];
+    }
+}
