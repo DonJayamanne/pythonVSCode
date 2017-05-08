64cfa54b9f31f554607262b25d3991a9f6eda220
Don Jayamanne <don.jayamanne@yahoo.com>
2017-05-08 22:16:25 +1000

    Remove commented code

--- src/client/jedi/commands.ts
+++ src/client/jedi/commands.ts
@@ -1,32 +1,24 @@
 "use strict";
 
-export class Commands {
+export class RequestCommands {
     public static Exit: Buffer = new Buffer("exit");
     public static Ping: Buffer = new Buffer("ping");
     public static Arguments = new Buffer("args");
     public static Completions = new Buffer("comp");
     public static Definitions = new Buffer("defs");
     public static Hover = new Buffer("hovr");
-    public static Usages = new Buffer("usaaag");
+    public static Usages = new Buffer("usag");
     public static Names = new Buffer("name");
 }
 
 export namespace ResponseCommands {
     export const Pong = 'pong';
+    export const TraceLog = 'tlog';
     export const Error = 'eror';
-    export const Arguments = "args";
+    export const Signature = "args";
     export const Completions = "comp";
     export const Definitions = "defs";
     export const Hover = "hovr";
-    export const Usages = "usag";
-    export const Names = "name";
+    export const References = "usag";
+    export const DocumentSymbols = "name";
 }
-
-/*
-commandNames.set(CommandType.Arguments, "arguments");
-commandNames.set(CommandType.Completions, "completions");
-commandNames.set(CommandType.Definitions, "definitions");
-commandNames.set(CommandType.Hover, "tooltip");
-commandNames.set(CommandType.Usages, "usages");
-commandNames.set(CommandType.Symbols, "names");
-*/
