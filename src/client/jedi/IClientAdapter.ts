b343a314510ef5d06fe5322a2f78e2db4865169c
Don Jayamanne <don.jayamanne@yahoo.com>
2017-05-08 21:51:56 +1000

    changes to interface

--- src/client/jedi/IClientAdapter.ts
+++ src/client/jedi/IClientAdapter.ts
@@ -4,10 +4,10 @@ import { TextDocument, Position, CancellationToken } from 'vscode';
 import { CompletionItem, Definition, Hover, ReferenceContext, Location, SignatureHelp, SymbolInformation } from 'vscode';
 
 export interface IClientAdapter {
-    getCompletions(document: TextDocument, position: Position, token: CancellationToken): Promise<CompletionItem[]>;
-    getDefinition(document: TextDocument, position: Position, token: CancellationToken): Promise<Definition>;
-    getHoverDefinition(document: TextDocument, position: Position, token: CancellationToken): Promise<Hover>;
-    getReferences(document: TextDocument, position: Position, context: ReferenceContext, token: CancellationToken): Promise<Location[]>;
-    getSignature(document: TextDocument, position: Position, token: CancellationToken): Promise<SignatureHelp>;
-    getDocumentSymbols(document: TextDocument, token: CancellationToken): Promise<SymbolInformation[]>;
+    getCompletions(token: CancellationToken, fileName: string, columnIndex: number, lineIndex: number, source: string): Promise<CompletionItem[]>;
+    getDefinition(token: CancellationToken, fileName: string, columnIndex: number, lineIndex: number, source: string): Promise<Definition>;
+    getHoverDefinition(token: CancellationToken, fileName: string, columnIndex: number, lineIndex: number, source: string): Promise<Hover>;
+    getReferences(token: CancellationToken, fileName: string, columnIndex: number, lineIndex: number, source: string): Promise<Location[]>;
+    getSignature(token: CancellationToken, fileName: string, columnIndex: number, lineIndex: number, source: string): Promise<SignatureHelp>;
+    getDocumentSymbols(token: CancellationToken, fileName: string, columnIndex: number, lineIndex: number, source: string): Promise<SymbolInformation[]>;
 }
