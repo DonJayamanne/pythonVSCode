055a53550c2c7aea713043b5b72e591fe8e071e0
Don Jayamanne <don.jayamanne@yahoo.com>
2017-05-08 21:01:16 +1000

    changes to socket client

--- src/client/jedi/commands.ts
+++ src/client/jedi/commands.ts
@@ -1,25 +1,32 @@
 "use strict";
 
 export class Commands {
-    public static ExitCommandBytes: Buffer = new Buffer("exit");
-    public static PingBytes: Buffer = new Buffer("ping");
-    public static ListKernelSpecsBytes: Buffer = new Buffer("lsks");
-    public static StartKernelBytes: Buffer = new Buffer("strk");
-    public static ShutdownKernelBytes: Buffer = new Buffer("stpk");
-    public static RestartKernelBytes: Buffer = new Buffer("rstk");
-    public static InterruptKernelBytes: Buffer = new Buffer("itpk");
-    public static RunCodeBytes: Buffer = new Buffer("run ");
+    public static Exit: Buffer = new Buffer("exit");
+    public static Ping: Buffer = new Buffer("ping");
+    public static Arguments = new Buffer("args");
+    public static Completions = new Buffer("comp");
+    public static Definitions = new Buffer("defs");
+    public static Hover = new Buffer("hovr");
+    public static Usages = new Buffer("usaaag");
+    public static Names = new Buffer("name");
 }
 
 export namespace ResponseCommands {
-    export const Pong = 'PONG';
-    export const ListKernelsSpecs = 'LSKS';
-    export const Error = 'EROR';
-    export const KernelStarted = 'STRK';
-    export const KernelShutdown = 'STPK';
-    export const KernelRestarted = 'RSTK';
-    export const KernelInterrupted = 'ITPK';
-    export const RunCode = 'RUN ';
-    export const ShellResult = 'SHEL';
-    export const IOPUBMessage = 'IOPB';
+    export const Pong = 'pong';
+    export const Error = 'eror';
+    export const Arguments = "args";
+    export const Completions = "comp";
+    export const Definitions = "defs";
+    export const Hover = "hovr";
+    export const Usages = "usag";
+    export const Names = "name";
 }
+
+/*
+commandNames.set(CommandType.Arguments, "arguments");
+commandNames.set(CommandType.Completions, "completions");
+commandNames.set(CommandType.Definitions, "definitions");
+commandNames.set(CommandType.Hover, "tooltip");
+commandNames.set(CommandType.Usages, "usages");
+commandNames.set(CommandType.Symbols, "names");
+*/
--- src/client/jedi/contracts.ts
+++ src/client/jedi/contracts.ts
@@ -1,16 +1,8 @@
 "use strict";
 
-import { KernelspecMetadata, Kernelspec, ParsedIOMessage } from '../contracts';
 import * as Rx from 'rx';
 
 export interface IJupyterClientAdapter {
-    getAllKernelSpecs(): Promise<{ [key: string]: Kernelspec }>;
-    startKernel(kernelSpec: KernelspecMetadata): Promise<[string, any, string]>;
-    shutdownkernel(kernelUUID: string): Promise<any>;
-    interruptKernel(kernelUUID: string): Promise<any>;
-    restartKernel(kernelUUID: string): Promise<any>;
-    runCode(code: string): Rx.IObservable<ParsedIOMessage>;
-    on(event: string | symbol, listener: Function): this;
 }
 
 export enum KernelCommand {
