ebf75e79975690ab1b631c0ae283df2547a99c08
Don Jayamanne <don.jayamanne@yahoo.com>
2017-06-29 22:26:57 +1000

    fix #1043

--- src/client/workspaceSymbols/generator.ts
+++ src/client/workspaceSymbols/generator.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
+import * as fs from 'fs';
 import * as child_process from 'child_process';
 import { PythonSettings } from '../common/configSettings';
 
@@ -42,7 +43,10 @@ export class Generator implements vscode.Disposable {
             outputFile = path.basename(outputFile);
         }
         outputFile = outputFile.indexOf(' ') > 0 ? `"${outputFile}"` : outputFile;
-
+        const outputDir = path.dirname(outputFile);
+        if (!fs.existsSync(outputDir)){
+            fs.mkdirSync(outputDir);
+        }
         args.push(`-o ${outputFile}`, '.');
         this.output.appendLine('-'.repeat(10) + 'Generating Tags' + '-'.repeat(10));
         this.output.appendLine(`${cmd} ${args.join(' ')}`);
