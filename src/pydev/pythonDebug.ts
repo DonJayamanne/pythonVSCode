d1cf6bfc0df76abae0a85bb0bf654cf7db7d94a4
xoviat <xoviat@users.noreply.github.com>
2017-07-02 18:54:26 -0500

    pythondebug: cleanup

--- src/pydev/pythonDebug.ts
+++ src/pydev/pythonDebug.ts
@@ -34,49 +34,108 @@ export function logError(...args: any[]) {
 	logger.error(logArgsToString(args));
 }
 
-/**
- * This interface should always match the schema found in the mock-debug extension manifest.
- */
+// This interface should always match the schema found in `package.json`.
 export interface LaunchRequestArguments extends DebugProtocol.LaunchRequestArguments {
-	/** An absolute path to the program to debug. */
 	program: string;
-	/** Automatically stop target after launch. If not specified, target does not stop. */
 	stopOnEntry?: boolean;
-	/** enable logging the Debug Adapter Protocol */
-	trace?: boolean;
+	args?: string[];
+	showLog?: boolean;
+	cwd?: string;
+	env?: { [key: string]: string; };
+	mode?: string;
+	remotePath?: string;
+	port?: number;
+	host?: string;
+	buildFlags?: string;
+	init?: string;
+	trace?: boolean | 'verbose';
+	/** Optional path to .env file. */
+	envFile?: string;
+	backend?: string;
+}
+
+interface DebuggerState {
+	exited: boolean;
+	exitStatus: number;
+	breakPoint: DebugBreakpoint;
+	breakPointInfo: {};
+	breakpointId: number;
+	currentThread: DebugThread;
+}
+
+interface DebugBreakpoint {
+	addr: number;
+	continue: boolean;
+	file: string;
+	functionName?: string;
+	id: number;
+	line: number;
+	stacktrace: number;
+	variables?: DebugVariable[];
+}
+
+interface DebugThread {
+	file: string;
+	id: number;
+	line: number;
+	pc: number;
+	function?: DebugFunction;
+};
+
+interface DebugLocation {
+	pc: number;
+	file: string;
+	line: number;
+	function: DebugFunction;
+}
+
+interface DebugFunction {
+	name: string;
+	value: number;
+	type: number;
+	goType: number;
+	args: DebugVariable[];
+	locals: DebugVariable[];
+}
+
+interface DebugVariable {
+	name: string;
+	addr: number;
+	type: string;
+	realType: string;
+	value: string;
+	len: number;
+	cap: number;
+	children: DebugVariable[];
+	unreadable: string;
+}
+
+class Deferred<T> extends Promise<T> {
+	public resolve: (value: T) => void
+	public reject: (reason?: any) => void
+	constructor() {
+		let that = {
+			resolve: null,
+			reject: null,
+		};
+		super((resolve, reject) => {
+			that.resolve = resolve;
+			that.reject = reject;
+		});
+		this.resolve = that.resolve;
+		this.reject = that.reject;
+	}
 }
 
 class PythonDebugSession extends LoggingDebugSession {
 
-	// we don't support multiple threads, so we can use a hardcoded ID for the default thread
-	private static THREAD_ID = 1;
+	private _variableHandles: Handles<DebugVariable>;
+	private breakpoints: Map<string, DebugBreakpoint[]>;
+	private threads: Set<number>;
+	private debugState: DebuggerState;
+	private pydevd: Deferred<PydevDebugger>;
 
-	// since we want to send breakpoint events, we will assign an id to every event
-	// so that the frontend can match events with breakpoints.
-	private _breakpointId = 1;
-
-	// This is the next line that will be 'executed'
-	private __currentLine = 0;
-	private get _currentLine(): number {
-		return this.__currentLine;
-	}
-	private set _currentLine(line: number) {
-		this.__currentLine = line;
-		this.log('line', line);
-	}
-
-	// the initial (and one and only) file we are 'debugging'
-	private _sourceFile: string;
-
-	// the contents (= lines) of the one and only file
-	private _sourceLines = new Array<string>();
-
-	// maps from sourceFile to array of Breakpoints
-	private _breakPoints = new Map<string, DebugProtocol.Breakpoint[]>();
-
-	private _variableHandles = new Handles<string>();
-
-	private pydevd: PydevDebugger;
+	private launchArgs: LaunchRequestArguments;
 
 	/**
 	 * Creates a new debug adapter that is used for one debug session.
@@ -88,6 +147,12 @@ class PythonDebugSession extends LoggingDebugSession {
 		// this debugger uses zero-based lines and columns
 		this.setDebuggerLinesStartAt1(false);
 		this.setDebuggerColumnsStartAt1(false);
+
+		this._variableHandles = new Handles<DebugVariable>();
+		this.threads = new Set<number>();
+		this.debugState = null;
+		this.pydevd = null;
+		this.breakpoints = new Map<string, DebugBreakpoint[]>();
 	}
 
 	/**
@@ -95,11 +160,22 @@ class PythonDebugSession extends LoggingDebugSession {
 	 * to interrogate the features the debug adapter provides.
 	 */
 	protected initializeRequest(response: DebugProtocol.InitializeResponse, args: DebugProtocol.InitializeRequestArguments): void {
-
+		verbose('InitializeRequest');
 		// since this debug adapter can accept configuration requests like 'setBreakpoint' at any time,
 		// we request them early by sending an 'initializeRequest' to the frontend.
 		// The frontend will end the configuration sequence by calling 'configurationDone' request.
 		this.sendEvent(new InitializedEvent());
+
+		this.debugState = {
+			exited: false,
+			exitStatus: null,
+			breakPoint: null,
+			breakPointInfo: {},
+			breakpointId: 0,
+			currentThread: null
+		};
+
+		this.pydevd = new Deferred();
 
 		response.body = response.body || {};
 		response.body.supportsConfigurationDoneRequest = true; // This debug adapter implements the configurationDoneRequest.
@@ -107,97 +183,72 @@ class PythonDebugSession extends LoggingDebugSession {
 		response.body.supportsStepBack = false; // Pydev does not support 'step back'
 
 		this.sendResponse(response);
+		verbose('InitializeResponse');
 	}
 
+	/**
+	 * MUST create a new pydevd instance.
+	 */
 	protected launchRequest(response: DebugProtocol.LaunchResponse, args: LaunchRequestArguments): void {
-		let port = 0; // Autoset port number
-		let localhost = '127.0.0.1';
+		this.launchArgs = args;
 
-		this.pydevd = new PydevDebugger(port, localhost, args.program, args);
-		this.pydevd.on('call', (command: Command, sequence: number, args) => {
-			switch (command) {
-				case Command.CMD_VERSION:
-					break
-				case Command.CMD_THREAD_CREATE:
-					break
-			}
-		});
-		this.pydevd.start();
-		this.pydevd.call(Command.CMD_RUN);
+		let port = args.port || 0; // Autoset the port number by default.
+		let host = args.host || '127.0.0.1';
+
+		this.pydevd.resolve(new PydevDebugger(port, host, args.program, args));
+		this.pydevd.then(pydevd => {
+			pydevd.on('call', (command: Command, sequence: number, args) => {
+				this.handleEvent(command, sequence, args);
+			});
+			pydevd.start();
+			pydevd.call(Command.CMD_RUN);
+		})
 
 		// make sure to 'Stop' the buffered logging if 'trace' is not set
-		logger.setup(args.trace ? Logger.LogLevel.Verbose : Logger.LogLevel.Stop, false);
+		// logger.setup(args.trace ? Logger.LogLevel.Verbose : Logger.LogLevel.Stop, false);
 
-		this._sourceFile = args.program;
-		this._sourceLines = readFileSync(this._sourceFile).toString().split('\n');
+		logger.setup(Logger.LogLevel.Verbose, false);
+	}
 
-		if (args.stopOnEntry) {
-			this._currentLine = 0;
-			this.sendResponse(response);
-
-			// we stop on the first line
-			this.sendEvent(new StoppedEvent("entry", PythonDebugSession.THREAD_ID));
-		} else {
-			// we just start to run until we hit a breakpoint or an exception
-			this.continueRequest(<DebugProtocol.ContinueResponse>response, { threadId: PythonDebugSession.THREAD_ID });
+	private handleEvent(command: Command, sequence: number, args) {
+		// Handle aribitrary commands
+		switch (command) {
+			case Command.CMD_VERSION:
+				break
+			case Command.CMD_THREAD_CREATE:
+				break
 		}
 	}
 
 	protected setBreakPointsRequest(response: DebugProtocol.SetBreakpointsResponse, args: DebugProtocol.SetBreakpointsArguments): void {
-
-		// breakpoint_id, 'python-line', self.get_main_filename(), line, func)
-
-		const path = <string>args.source.path;
-		const clientLines = args.lines || [];
-
-		// read file contents into array for direct access
-		const lines = readFileSync(path).toString().split('\n');
-		const breakpoints = new Array<Breakpoint>();
-
-		this._breakPoints.get(path).map(existingBP => {
-			verbose('Clearing: ' + existingBP.id);
-			return this.pydevd.call(Command.CMD_REMOVE_BREAK, ['python-line', path, existingBP.id]);
-		});
-
-		args.lines.map(line => {
-			verbose('Creating on: ' + path + line);
-			this._breakpointId++;
-			this.pydevd.call(Command.CMD_SET_BREAK, [this._breakpointId, 'python-line', 'None', 'None', 'None']);
-		});
-
-		// TODO: replace this boilerplate code
-
-		// verify breakpoint locations
-		for (let i = 0; i < clientLines.length; i++) {
-			let l = this.convertClientLineToDebugger(clientLines[i]);
-			let verified = false;
-			if (l < lines.length) {
-				const line = lines[l].trim();
-				// if a line is empty or starts with '+' we don't allow to set a breakpoint but move the breakpoint down
-				if (line.length === 0 || line.indexOf("+") === 0) {
-					l++;
-				}
-				// if a line starts with '-' we don't allow to set a breakpoint but move the breakpoint up
-				if (line.indexOf("-") === 0) {
-					l--;
-				}
-				// don't set 'verified' to true if the line contains the word 'lazy'
-				// in this case the breakpoint will be verified 'lazy' after hitting it once.
-				if (line.indexOf("lazy") < 0) {
-					verified = true;    // this breakpoint has been validated
-				}
-			}
-			const bp = <DebugProtocol.Breakpoint>new Breakpoint(verified, this.convertDebuggerLineToClient(l));
-			bp.id = this._breakpointId++;
-			breakpoints.push(bp);
+		verbose('SetBreakPointsRequest');
+		if (!this.breakpoints.get(args.source.path)) {
+			this.breakpoints.set(args.source.path, []);
 		}
-		this._breakPoints.set(path, breakpoints);
+		// breakpoint_id, 'python-line', self.get_main_filename(), line, func)
+		let file = args.source.path;
 
-		// send back the actual breakpoint positions
-		response.body = {
-			breakpoints: breakpoints
-		};
-		this.sendResponse(response);
+		this.pydevd.then(pydevd => {
+			this.breakpoints.get(file).map(existingBP => {
+				verbose('Clearing: ' + existingBP.id);
+				return pydevd.call(Command.CMD_REMOVE_BREAK, ['python-line', args.source.path, existingBP.id]);
+			});
+
+			args.lines.map(line => {
+				verbose('Creating on: ' + file + ':' + line);
+
+				this.debugState.breakpointId++;
+				return pydevd.call(Command.CMD_SET_BREAK, [this.debugState.breakpointId, 'python-line', file, line, 'None']);
+			})
+
+			let breakpoints = args.lines.map(line => {
+				return { verified: false, line: line };
+			})
+
+			response.body = { breakpoints };
+			this.sendResponse(response);
+			verbose('SetBreakPointsResponse');
+		});
 	}
 
 	protected threadsRequest(response: DebugProtocol.ThreadsResponse): void {
@@ -208,12 +259,6 @@ class PythonDebugSession extends LoggingDebugSession {
 			});
 		*/
 
-		// return the default thread
-		response.body = {
-			threads: [
-				new Thread(PythonDebugSession.THREAD_ID, "thread 1")
-			]
-		};
 		this.sendResponse(response);
 	}
 
@@ -222,85 +267,21 @@ class PythonDebugSession extends LoggingDebugSession {
 	 */
 	protected stackTraceRequest(response: DebugProtocol.StackTraceResponse, args: DebugProtocol.StackTraceArguments): void {
 
-		const words = this._sourceLines[this._currentLine].trim().split(/\s+/);
-
-		const startFrame = typeof args.startFrame === 'number' ? args.startFrame : 0;
-		const maxLevels = typeof args.levels === 'number' ? args.levels : words.length - startFrame;
-		const endFrame = Math.min(startFrame + maxLevels, words.length);
-
-		const frames = new Array<StackFrame>();
-		// every word of the current line becomes a stack frame.
-		for (let i = startFrame; i < endFrame; i++) {
-			const name = words[i];	// use a word of the line as the stackframe name
-			frames.push(new StackFrame(i, `${name}(${i})`, new Source(basename(this._sourceFile),
-				this.convertDebuggerPathToClient(this._sourceFile)),
-				this.convertDebuggerLineToClient(this._currentLine), 0));
-		}
-		response.body = {
-			stackFrames: frames,
-			totalFrames: words.length
-		};
 		this.sendResponse(response);
 	}
 
 	protected scopesRequest(response: DebugProtocol.ScopesResponse, args: DebugProtocol.ScopesArguments): void {
 
-		const frameReference = args.frameId;
-		const scopes = new Array<Scope>();
-		scopes.push(new Scope("Local", this._variableHandles.create("local_" + frameReference), false));
-		scopes.push(new Scope("Closure", this._variableHandles.create("closure_" + frameReference), false));
-		scopes.push(new Scope("Global", this._variableHandles.create("global_" + frameReference), true));
-
-		response.body = {
-			scopes: scopes
-		};
 		this.sendResponse(response);
 	}
 
 	protected variablesRequest(response: DebugProtocol.VariablesResponse, args: DebugProtocol.VariablesArguments): void {
 
-		const variables = new Array<DebugProtocol.Variable>();
-		const id = this._variableHandles.get(args.variablesReference);
-		if (id !== null) {
-			variables.push({
-				name: id + "_i",
-				type: "integer",
-				value: "123",
-				variablesReference: 0
-			});
-			variables.push({
-				name: id + "_f",
-				type: "float",
-				value: "3.14",
-				variablesReference: 0
-			});
-			variables.push({
-				name: id + "_s",
-				type: "string",
-				value: "hello world",
-				variablesReference: 0
-			});
-			variables.push({
-				name: id + "_o",
-				type: "object",
-				value: "Object",
-				variablesReference: this._variableHandles.create("object_")
-			});
-		}
-
-		response.body = {
-			variables: variables
-		};
 		this.sendResponse(response);
 	}
 
 	protected continueRequest(response: DebugProtocol.ContinueResponse, args: DebugProtocol.ContinueArguments): void {
 
-		for (let ln = this._currentLine + 1; ln < this._sourceLines.length; ln++) {
-			if (this.fireEventsForLine(response, ln)) {
-				return;
-			}
-		}
 		this.sendResponse(response);
 		// no more lines: run to end
 		this.sendEvent(new TerminatedEvent());
@@ -308,96 +289,30 @@ class PythonDebugSession extends LoggingDebugSession {
 
 	protected nextRequest(response: DebugProtocol.NextResponse, args: DebugProtocol.NextArguments): void {
 
-		this.pydevd.call(Command.CMD_STEP_OVER);
+		// this.pydevd.call(Command.CMD_STEP_OVER);
 
-		for (let ln = this._currentLine + 1; ln < this._sourceLines.length; ln++) {
-			if (this.fireStepEvent(response, ln)) {
-				return;
-			}
-		}
 		this.sendResponse(response);
 		// no more lines: run to end
 		this.sendEvent(new TerminatedEvent());
 	}
 
 	protected stepInRequest(response: DebugProtocol.StepInResponse): void {
-		this.pydevd.call(Command.CMD_STEP_INTO);
+
+		// this.pydevd.call(Command.CMD_STEP_INTO);
+		this.sendResponse(response);
 	}
 
 	protected stepOutRequest(response: DebugProtocol.StepOutResponse): void {
-		this.pydevd.call(Command.CMD_STEP_RETURN);
+
+		// this.pydevd.call(Command.CMD_STEP_RETURN);
+		this.sendResponse(response);
 	}
 
 	protected evaluateRequest(response: DebugProtocol.EvaluateResponse, args: DebugProtocol.EvaluateArguments): void {
 
-		response.body = {
-			result: `evaluate(context: '${args.context}', '${args.expression}')`,
-			variablesReference: 0
-		};
 		this.sendResponse(response);
 	}
 
-	//---- some helpers
-
-	/**
-	 * Fire StoppedEvent if line is not empty.
-	 */
-	private fireStepEvent(response: DebugProtocol.Response, ln: number): boolean {
-
-		if (this._sourceLines[ln].trim().length > 0) {	// non-empty line
-			this._currentLine = ln;
-			this.sendResponse(response);
-			this.sendEvent(new StoppedEvent("step", PythonDebugSession.THREAD_ID));
-			return true;
-		}
-		return false;
-	}
-
-	/**
-	 * Fire StoppedEvent if line has a breakpoint or the word 'exception' is found.
-	 */
-	private fireEventsForLine(response: DebugProtocol.Response, ln: number): boolean {
-
-		// find the breakpoints for the current source file
-		const breakpoints = this._breakPoints.get(this._sourceFile);
-		if (breakpoints) {
-			const bps = breakpoints.filter(bp => bp.line === this.convertDebuggerLineToClient(ln));
-			if (bps.length > 0) {
-				this._currentLine = ln;
-
-				// 'continue' request finished
-				this.sendResponse(response);
-
-				// send 'stopped' event
-				this.sendEvent(new StoppedEvent("breakpoint", PythonDebugSession.THREAD_ID));
-
-				// the following shows the use of 'breakpoint' events to update properties of a breakpoint in the UI
-				// if breakpoint is not yet verified, verify it now and send a 'breakpoint' update event
-				if (!bps[0].verified) {
-					bps[0].verified = true;
-					this.sendEvent(new BreakpointEvent("update", bps[0]));
-				}
-				return true;
-			}
-		}
-
-		// if word 'exception' found in source -> throw exception
-		if (this._sourceLines[ln].indexOf("exception") >= 0) {
-			this._currentLine = ln;
-			this.sendResponse(response);
-			this.sendEvent(new StoppedEvent("exception", PythonDebugSession.THREAD_ID));
-			this.log('exception in line', ln);
-			return true;
-		}
-
-		return false;
-	}
-
-	private log(msg: string, line: number) {
-		const e = new OutputEvent(`${msg}: ${line}\n`);
-		(<DebugProtocol.OutputEvent>e).body.variablesReference = this._variableHandles.create("args");
-		this.sendEvent(e);	// print current line on debug console
-	}
 }
 
 DebugSession.run(PythonDebugSession);
