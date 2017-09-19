3f1774b4b4c26e7879431d24a367aa60c1dce140
Don Jayamanne <don.jayamanne@yahoo.com>
2017-09-19 14:22:45 -0700

    fixed conda tests

--- src/test/interpreters/condaEnvProvider.test.ts
+++ src/test/interpreters/condaEnvProvider.test.ts
@@ -30,63 +30,71 @@ suite('Interpreters from Conda Environments', () => {
     });
     test('Must extract display name from version info', async () => {
         const condaProvider = new CondaEnvProvider();
+        const windowsEnvs = ['c:\\temp', 'c:\\two three\\four'];
+        const nonWindowsEnvs = ['usr/temp', 'usr/two three/four'];
         const info = {
-            envs: ['c:\\temp', 'c:\\two three\\four'],
+            envs: IS_WINDOWS ? windowsEnvs : nonWindowsEnvs,
             default_prefix: '',
             'sys.version': '3.6.1 |Anaconda 4.4.0 (64-bit)| (default, May 11 2017, 13:25:24) [MSC v.1900 64 bit (AMD64)]'
         };
         const interpreters = await condaProvider.parseCondaInfo(info);
         assert.equal(interpreters.length, 2, 'Incorrect number of entries');
 
-        const path1 = path.join(info.envs[0], IS_WINDOWS ? 'python.exe' : 'python');
+        const path1 = path.join(info.envs[0], IS_WINDOWS ? 'python.exe' : 'bin/python');
         assert.equal(interpreters[0].path, path1, 'Incorrect path for first env');
         assert.equal(interpreters[0].displayName, 'Anaconda 4.4.0 (64-bit) (temp)', 'Incorrect display name for first env');
         assert.equal(interpreters[0].companyDisplayName, 'Continuum Analytics, Inc.', 'Incorrect company display name for first env');
 
-        const path2 = path.join(info.envs[1], IS_WINDOWS ? 'python.exe' : 'python');
+        const path2 = path.join(info.envs[1], IS_WINDOWS ? 'python.exe' : 'bin/python');
         assert.equal(interpreters[1].path, path2, 'Incorrect path for first env');
         assert.equal(interpreters[1].displayName, 'Anaconda 4.4.0 (64-bit) (four)', 'Incorrect display name for first env');
         assert.equal(interpreters[1].companyDisplayName, 'Continuum Analytics, Inc.', 'Incorrect company display name for first env');
     });
     test('Must use the default display name if sys.version is invalid', async () => {
         const condaProvider = new CondaEnvProvider();
+        const windowsEnvs = ['c:\\temp'];
+        const nonWindowsEnvs = ['usr/temp'];
         const info = {
-            envs: ['c:\\temp'],
+            envs: IS_WINDOWS ? windowsEnvs : nonWindowsEnvs,
             default_prefix: '',
             'sys.version': '3.6.1 |Anaonda 4.4.0 (64-bit)| (default, May 11 2017, 13:25:24) [MSC v.1900 64 bit (AMD64)]'
         };
         const interpreters = await condaProvider.parseCondaInfo(info);
         assert.equal(interpreters.length, 1, 'Incorrect number of entries');
 
-        const path1 = path.join(info.envs[0], IS_WINDOWS ? 'python.exe' : 'python');
+        const path1 = path.join(info.envs[0], IS_WINDOWS ? 'python.exe' : 'bin/python');
         assert.equal(interpreters[0].path, path1, 'Incorrect path for first env');
         assert.equal(interpreters[0].displayName, 'Anaconda (temp)', 'Incorrect display name for first env');
         assert.equal(interpreters[0].companyDisplayName, 'Continuum Analytics, Inc.', 'Incorrect company display name for first env');
     });
     test('Must use the default display name if sys.version is empty', async () => {
         const condaProvider = new CondaEnvProvider();
+        const windowsEnvs = ['c:\\temp'];
+        const nonWindowsEnvs = ['usr/temp'];
         const info = {
-            envs: ['c:\\temp']
+            envs: IS_WINDOWS ? windowsEnvs : nonWindowsEnvs,
         };
         const interpreters = await condaProvider.parseCondaInfo(info as any);
         assert.equal(interpreters.length, 1, 'Incorrect number of entries');
 
-        const path1 = path.join(info.envs[0], IS_WINDOWS ? 'python.exe' : 'python');
+        const path1 = path.join(info.envs[0], IS_WINDOWS ? 'python.exe' : 'bin/python');
         assert.equal(interpreters[0].path, path1, 'Incorrect path for first env');
         assert.equal(interpreters[0].displayName, 'Anaconda (temp)', 'Incorrect display name for first env');
         assert.equal(interpreters[0].companyDisplayName, 'Continuum Analytics, Inc.', 'Incorrect company display name for first env');
     });
     test('Must include the default_prefix into the list of interpreters', async () => {
         const condaProvider = new CondaEnvProvider();
+        const windowsEnvs = 'c:\\temp';
+        const nonWindowsEnvs = 'usr/temp';
         const info = {
-            default_prefix: 'c:\\xx\\yy'
+            default_prefix: IS_WINDOWS ? windowsEnvs : nonWindowsEnvs,
         };
         const interpreters = await condaProvider.parseCondaInfo(info as any);
         assert.equal(interpreters.length, 1, 'Incorrect number of entries');
 
-        const path1 = path.join(info.default_prefix, IS_WINDOWS ? 'python.exe' : 'python');
+        const path1 = path.join(info.default_prefix, IS_WINDOWS ? 'python.exe' : 'bin/python');
         assert.equal(interpreters[0].path, path1, 'Incorrect path for first env');
-        assert.equal(interpreters[0].displayName, 'Anaconda (yy)', 'Incorrect display name for first env');
+        assert.equal(interpreters[0].displayName, 'Anaconda (temp)', 'Incorrect display name for first env');
         assert.equal(interpreters[0].companyDisplayName, 'Continuum Analytics, Inc.', 'Incorrect company display name for first env');
     });
 });
