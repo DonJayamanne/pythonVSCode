965eae9272a03a280f3f2ce066c6f7f46cea9dc8
Don Jayamanne <don.jayamanne@yahoo.com>
2017-10-09 14:16:34 -0700

    oops

--- src/test/index.ts
+++ src/test/index.ts
@@ -18,8 +18,7 @@ let testRunner = require('vscode/lib/testrunner');
 testRunner.configure({
     ui: 'tdd', 		// the TDD UI is being used in extension.test.ts (suite, test, etc.)
     useColors: true, // colored output from test results
-    timeout: 25000,
-    grep: 'Configuration Settings'
+    timeout: 25000
 });
 
 initializePython();
