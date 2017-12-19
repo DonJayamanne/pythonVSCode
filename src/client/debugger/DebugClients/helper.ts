649b7e7037809fd091b691ef9fc5ca7e33ed1261
Don Jayamanne <don.jayamanne@yahoo.com>
2017-12-19 14:09:20 -0800

    console could be null

--- src/client/debugger/DebugClients/helper.ts
+++ src/client/debugger/DebugClients/helper.ts
@@ -30,7 +30,7 @@ export class DebugClientHelper {
             this.envParser.appendPythonPath(env, this.process.env.PYTHONPATH);
         }
 
-        if (args.console === 'none') {
+        if (typeof args.console !== 'string' || args.console === 'none') {
             // For debugging, when not using any terminal, then we need to provide all env variables.
             // As we're spawning the process, we need to ensure all env variables are passed.
             // Including those from the current process (i.e. everything, not just custom vars).
