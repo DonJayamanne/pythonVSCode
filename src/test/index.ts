a6525ab25bb596f91c69cf09dcd31e9009b48d5a
Don Jayamanne <don.jayamanne@yahoo.com>
2017-12-21 22:42:17 -0800

    run all tests

--- src/test/index.ts
+++ src/test/index.ts
@@ -18,8 +18,7 @@ const options: MochaSetupOptions & { retries: number } = {
     ui: 'tdd',
     useColors: true,
     timeout: 25000,
-    retries: 3,
-    grep: 'Installer'
+    retries: 3
 };
 testRunner.configure(options, { coverageConfig: '../coverconfig.json' });
 module.exports = testRunner;
