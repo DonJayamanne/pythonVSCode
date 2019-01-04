baea97975333de6b3b7c087448796a1e468b5be9
Don Jayamanne <don.jayamanne@yahoo.com>
2019-01-04 08:49:39 -0800

    Fix number type

--- src/client/common/utils/text.ts
+++ src/client/common/utils/text.ts
@@ -6,7 +6,7 @@
 import { Position, Range, TextDocument } from 'vscode';
 import { isNumber } from './sysTypes';
 
-export function getWindowsLineEndingCount(document: TextDocument, offset: Number) {
+export function getWindowsLineEndingCount(document: TextDocument, offset: number) {
     //const eolPattern = new RegExp('\r\n', 'g');
     const eolPattern = /\r\n/g;
     const readBlock = 1024;
