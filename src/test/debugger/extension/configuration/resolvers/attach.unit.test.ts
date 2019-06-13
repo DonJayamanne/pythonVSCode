a4d7bce4f1a2f8f257d73c19f898a2a17fc48eff
Kim-Adeline Miguel <kimiguel@microsoft.com>
2019-06-13 15:25:22 -0700

    Added unit test

--- src/test/debugger/extension/configuration/resolvers/attach.unit.test.ts
+++ src/test/debugger/extension/configuration/resolvers/attach.unit.test.ts
@@ -168,6 +168,23 @@ getNamesAndValues(OSType).forEach(os => {
                 expect(pathMappings![0].localRoot).to.be.equal(workspaceFolder.uri.fsPath);
                 expect(pathMappings![0].remoteRoot).to.be.equal(workspaceFolder.uri.fsPath);
             });
+
+            if (os.name === 'Windows') {
+                test(`Ensure drive letter is lower cased for local path mappings on Windows when host is ${host}`, async () => {
+                    const activeFile = 'xyz.py';
+                    const workspaceFolder = createMoqWorkspaceFolder('C:/Debug/Python_Path');
+                    setupActiveEditor(activeFile, PYTHON_LANGUAGE);
+                    const defaultWorkspace = path.join('usr', 'desktop');
+                    setupWorkspaces([defaultWorkspace]);
+
+                    const localRoot = `Debug_PythonPath_${new Date().toString()}`;
+                    const debugConfig = await debugProvider.resolveDebugConfiguration!(workspaceFolder, { localRoot, host, request: 'attach' } as any as DebugConfiguration);
+                    const pathMappings = (debugConfig as AttachRequestArguments).pathMappings;
+                    const lowercasedLocalRoot = workspaceFolder.uri.fsPath.charAt(0).toLowerCase() + workspaceFolder.uri.fsPath.substr(1);
+
+                    expect(pathMappings![0].localRoot).to.be.equal(lowercasedLocalRoot);
+                });
+            }
         });
         ['192.168.1.123', 'don.debugger.com'].forEach(host => {
             test(`Ensure path mappings are not automatically added when host is '${host}'`, async () => {
