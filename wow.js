35c5be70ad8662f57cc65b8ae7df960ba5ecbfe6
Don Jayamanne <don.jayamanne@yahoo.com>
2020-04-24 08:52:22 -0700

    Added tests (works on unix)

--- wow.js
+++ wow.js
@@ -1,10 +1,13 @@
 const childprocess = require('child_process');
 
 const proc = childprocess.spawn(
-    '/Users/donjayamanne/Desktop/Development/crap/docBug/venvForWidgets/bin/python',
-    ['wow.py'],
+    // '/Users/donjayamanne/Desktop/Development/crap/docBug/venvForWidgets/bin/python',
+    '/Users/donjayamanne/Desktop/Development/crap/docBug/venvForDS/bin/python',
+    ['/Users/donjayamanne/Desktop/Development/vsc/pythonVSCode/wow.py'],
+    // ['-m', 'ipykernel_launcher', '-f', '/Users/donjayamanne/Desktop/Development/vsc/pythonVSCode/wow.json'],
     {
         env: {
+            // ...process.env,
             PYTHONUNBUFFERED: '1',
             PYTHONPATH: '/Users/donjayamanne/Desktop/Development/vsc/pythonVSCode/pythonFiles'
         }
--- wow.py
+++ wow.py
@@ -6,7 +6,7 @@ import sys
 import subprocess
 import threading
 
-from vscode_datascience_helpers.kernel_launcher import launch_kernel
+# from vscode_datascience_helpers.kernel_launcher import launch_kernel
 log = logging.getLogger(__name__)
 root_logger = logging.root
 LOG_FORMAT = (
@@ -35,9 +35,10 @@ class PythonDaemon:
         self.kernel_thread.start()
 
     def _read_stderr_in_background(self):
+        print("Check Kaboom2")
         while self.kernel.poll() is None:
             self.kernel_started.set()
-            stderr_output = self.kernel.stderr.read(1)
+            stderr_output = self.kernel.stderr.read()
             if stderr_output:
                 # self.log.debug(
                 #     "subprocess output for, %s with args %s, has stderr_output %s",
@@ -45,22 +46,30 @@ class PythonDaemon:
                 #     stderr_output.decode("utf-8"),
                 #     stderr_output,
                 # )
-                sys.stderr.buffer.write(stderr_output)
+                print('stderr_output')
+                print(stderr_output)
+                # sys.stderr.write(stderr_output)
                 #sys.stderr.flush()
+        print("Kaboom2")
 
     def _read_stdout_in_background(self):
+        print("Check Kaboom")
         while self.kernel.poll() is None:
-            stdout_output = self.kernel.stdout.read(1)
+            print('stdout_output')
+            stdout_output = self.kernel.stdout.read()
+            print(stdout_output)
             if stdout_output:
+                print(stdout_output)
                 # self.log.debug(
                 #     "subprocess output for, %s with args %s, has stdout_output %s",
                 #     module_name,
                 #     stdout_output.decode("utf-8"),
                 #     stdout_output,
                 # )
-                sys.stdout.buffer.write(stdout_output)
-
+                # sys.stdout.write(stdout_output)
+        print("Kaboom")
     def _monitor_kernel(self):
+        print("Checking %s", self.kernel.pid)
         while self.kernel.poll() is None:
             pass
         print("Bye kernel output %s", self.kernel.pid)
@@ -88,28 +97,40 @@ class PythonDaemon:
             args = [] if args is None else args
             cmd = [sys.executable, "-m", module_name] + args
             env = os.environ.copy()
-            proc = launch_kernel(
-            # proc = subprocess.Popen(
-                cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, env=env, independent=True
+            print(env)
+            # with open('env.json', 'w') as fp:
+            #     json.dump(env, fp)
+            # env['PYTHONUNBUFFERED'] = '1'
+            # #env = {'SHELL': '/bin/zsh', 'SSH_AUTH_SOCK': '/private/tmp/com.apple.launchd.VGYkUZ66qF/Listeners', 'XPC_FLAGS': '0x0', 'XPC_SERVICE_NAME': '0', 'HOME': '/Users/donjayamanne', 'LOGNAME': 'donjayamanne', 'TMPDIR': '/var/folders/56/9h11nzxj6b78yvk72vsngk140000gn/T/', 'PATH': '/Users/donjayamanne/Desktop/Development/crap/docBug/venvForWidgets/bin:/Users/donjayamanne/.yarn/bin:/Users/donjayamanne/.config/yarn/global/node_modules/.bin:/Users/donjayamanne/.poetry/bin:/Users/donjayamanne/.poetry/bin:/Users/donjayamanne/.nvm/versions/node/v12.4.0/bin:/Users/donjayamanne/.local/share/virtualenvs:/Users/donjayamanne/.pyenv/shims:/Users/donjayamanne/Desktop/Development/software/anaconda/anaconda3/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/go/bin', 'TERM_PROGRAM': 'vscode', 'TERM_PROGRAM_VERSION': '1.45.0-insider', 'LANG': 'en_US.UTF-8', 'COLORTERM': 'truecolor', 'PWD': '/', 'TERM': 'xterm-256color', 'SHLVL': '1', 'OLDPWD': '/Users/donjayamanne/Desktop/Development/crap/docBug', 'ZSH': '/Users/donjayamanne/.oh-my-zsh', 'PAGER': 'less', 'LESS': '-R', 'LSCOLORS': 'Gxfxcxdxbxegedabagacad', 'CONDA_EXE': '/opt/miniconda3/bin/conda', '_CE_M': '', '_CE_CONDA': '', 'CONDA_PYTHON_EXE': '/opt/miniconda3/bin/python', 'CONDA_SHLVL': '0', 'LDFLAGS': '-L/usr/local/opt/readline/lib', 'CPPFLAGS': '-I/usr/local/opt/readline/include', 'PYENV_SHELL': 'zsh', 'NVM_DIR': '/Users/donjayamanne/.nvm', 'NVM_CD_FLAGS': '-q', 'NVM_BIN': '/Users/donjayamanne/.nvm/versions/node/v12.4.0/bin', 'VIRTUAL_ENV': '/Users/donjayamanne/Desktop/Development/crap/docBug/venvForWidgets', 'PS1': '(venvForWidgets) ', '_': '/Users/donjayamanne/Desktop/Development/crap/docBug/venvForWidgets/bin/python', '__CF_USER_TEXT_ENCODING': '0x1F5:0x0:0x0', 'KERNEL_LAUNCH_TIMEOUT': '40', 'BEAKERX_AUTOTRANSLATION_PASSWORD': 'iJ1Z5bPKcXRzPLZ27VOPweV0wlqalmJQIo1H3ByfYA3yXY7pyFd15ozum8Ld6FLtAuKEnqushYpqm3Uab0xMEGOsOIgNAYq1GRbsKttocZqbFoFDm5BpU0etEAdehFt5', 'BEAKERX_AUTOTRANSLATION_PORT': '63658', 'VSCODE_NODE_CACHED_DATA_DIR': '/Users/donjayamanne/Library/Application Support/Code/CachedData/a9f8623ec050e5f0b44cc8ce8204a1455884749f', 'AMD_ENTRYPOINT': 'vs/workbench/services/extensions/node/extensionHostProcess', 'PYTHONUNBUFFERED': '1', 'PYTHONIOENCODING': 'utf-8', 'USER': 'donjayamanne', 'COMMAND_MODE': 'unix2003', 'VSCODE_LOG_STACK': 'false', 'WOW': 'UPDATED', 'ELECTRON_RUN_AS_NODE': '1', 'VSCODE_LOGS': '/Users/donjayamanne/Library/Application Support/Code/logs/20200416T213050', 'LaunchInstanceID': '09EE0261-0BFC-44E3-BFAE-215BFEA1D9F4', 'VSCODE_HANDLES_UNCAUGHT_ERRORS': 'true', 'APPLICATION_INSIGHTS_NO_DIAGNOSTIC_CHANNEL': 'true', 'PIPE_LOGGING': 'true', 'VSCODE_IPC_HOOK_EXTHOST': '/var/folders/56/9h11nzxj6b78yvk72vsngk140000gn/T/vscode-ipc-53fdde40-274c-470b-a29a-b21d13e0cbea.sock', 'VSCODE_NLS_CONFIG': '{"locale":"en-us","availableLanguages":{},"_languagePackSupport":true}', 'PYTHONPATH': './one', 'THAT': 'ABC', 'VSCODE_IPC_HOOK': '/Users/donjayamanne/Library/Application Support/Code/1.44.1-main.sock', 'VSCODE_PID': '476', 'VERBOSE_LOGGING': 'true', 'SECURITYSESSIONID': '186a7', 'LC_CTYPE': 'UTF-8'}
+            # print(cmd)
+            # proc = launch_kernel(
+            proc = subprocess.Popen(
+                # cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, env=env, independent=True
+                cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd, env=env
+                # cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,env=env
+                # cmd, stdout=sys.stdout, stderr=sys.stderr,env=env
             )
         except:
+            import traceback
             self.kernel_start_error = traceback.format_exc()
             print("Failed starting kernel. Error: %s", self.kernel_start_error)
             sys.stderr.write(self.kernel_start_error)
             self.kernel_started.set()
             return
-
+        import time
+        time.sleep(5)
+        print(proc.stderr.read())
         self.kernel = proc
         print("Kernel launched, with PID %s", proc.pid)
 
-        threading.Thread(
-            target=self._read_stdout_in_background,
-            daemon=True,
-        ).start()
-        threading.Thread(
-            target=self._read_stderr_in_background,
-            daemon=True,
-        ).start()
+        # threading.Thread(
+        #     target=self._read_stdout_in_background,
+        #     daemon=True,
+        # ).start()
+        # threading.Thread(
+        #     target=self._read_stderr_in_background,
+        #     daemon=True,
+        # ).start()
         threading.Thread(
             target=self._monitor_kernel,
             daemon=True,
@@ -153,6 +174,6 @@ class PythonDaemon:
 import time
 print('start')
 d = PythonDaemon()
-d.exec_module_observable_in_background('ipykernel_launcher', ['-f', '/var/folders/56/9h11nzxj6b78yvk72vsngk140000gn/T/tmp-92647W1h56TYJUxOa.json'])
+d.exec_module_observable_in_background('ipykernel_launcher', ['-f', '/Users/donjayamanne/Desktop/Development/vsc/pythonVSCode/wow.json'])
 time.sleep(10)
 print('end')
