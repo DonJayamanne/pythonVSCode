f29c7d34cb5b65d7a96a95c92f21cbccf8ce83a4
Don Jayamanne <don.jayamanne@yahoo.com>
2020-06-10 09:10:32 -0700

    Remove unnecessary type

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,12 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-
-// Added to allow compilation of backbone types pulled in from ipywidgets (@jupyterlab/widgets).
-declare module JQuery {
-    type TriggeredEvent = any;
-}
-
 declare module '@phosphor/coreutils' {
     export class PromiseDelegate<T> {
         /**
@@ -71,7 +65,7 @@ declare module '@phosphor/coreutils' {
     /**
      * A type definition for a JSON array.
      */
-    export interface JSONArray extends Array<JSONValue> { }
+    export interface JSONArray extends Array<JSONValue> {}
     /**
      * A type definition for a readonly JSON object.
      */
@@ -81,7 +75,7 @@ declare module '@phosphor/coreutils' {
     /**
      * A type definition for a readonly JSON array.
      */
-    export interface ReadonlyJSONArray extends ReadonlyArray<ReadonlyJSONValue> { }
+    export interface ReadonlyJSONArray extends ReadonlyArray<ReadonlyJSONValue> {}
     /**
      * A type alias for a readonly JSON value.
      */
