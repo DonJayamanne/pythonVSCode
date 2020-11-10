DonJayamanne/pythonVSCode

commit 2a6761dc7713e629efe993087af138c2f41eb29f

Add more env-related helpers. (#14326)
The PR for "windows known paths locator" depends on this.

src/client/common/utils/platform.ts
@@ -30,15 +30,32 @@ export function getOSType(platform: string = process.platform): OSType {
     }
 }
 
+const architectures: Record<string, Architecture> = {
+    x86: Architecture.x86, // 32-bit
+    x64: Architecture.x64, // 64-bit
+    '': Architecture.Unknown
+};
+
+/**
+ * Identify the host's native architecture/bitness.
+ */
+export function getArchitecture(): Architecture {
+    return architectures[process.arch] || Architecture.Unknown;
+}
+
+/**
+ * Look up the requested env var value (or  undefined` if not set).
+ */
 export function getEnvironmentVariable(key: string): string | undefined {
     // tslint:disable-next-line: no-any
     return ((process.env as any) as EnvironmentVariables)[key];
 }
 
-export function getPathEnvironmentVariable(): string | undefined {
-    return getEnvironmentVariable('Path') || getEnvironmentVariable('PATH');
-}
-
+/**
+ * Get the current user's home directory.
+ *
+ * The lookup is limited to environment variables.
+ */
 export function getUserHomeDir(): string | undefined {
     if (getOSType() === OSType.Windows) {
         return getEnvironmentVariable('USERPROFILE');

src/client/common/platform/constants.ts
@@ -5,6 +5,4 @@
 // TODO : Drop all these in favor of IPlatformService.
 // See https://github.com/microsoft/vscode-python/issues/8542.
 
-export const WINDOWS_PATH_VARIABLE_NAME = 'Path';
-export const NON_WINDOWS_PATH_VARIABLE_NAME = 'PATH';
 export const IS_WINDOWS = /^win/.test(process.platform);

src/client/common/platform/fs-paths.ts
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 import * as nodepath from 'path';
+import { getSearchPathEnvVarNames } from '../utils/exec';
 import { getOSType, OSType } from '../utils/platform';
 import { IExecutables, IFileSystemPaths, IFileSystemPathUtils } from './types';
 // tslint:disable-next-line:no-var-requires no-require-imports
@@ -86,7 +87,7 @@ export class Executables {
     }
 
     public get envVar(): string {
-        return this.osType === OSType.Windows ? 'Path' : 'PATH';
+        return getSearchPathEnvVarNames(this.osType)[0];
     }
 }
 

src/client/common/platform/platformService.ts
@@ -7,9 +7,9 @@ import * as os from 'os';
 import { coerce, SemVer } from 'semver';
 import { sendTelemetryEvent } from '../../telemetry';
 import { EventName, PlatformErrors } from '../../telemetry/constants';
-import { getOSType, OSType } from '../utils/platform';
+import { getSearchPathEnvVarNames } from '../utils/exec';
+import { Architecture, getArchitecture, getOSType, OSType } from '../utils/platform';
 import { parseVersion } from '../utils/version';
-import { NON_WINDOWS_PATH_VARIABLE_NAME, WINDOWS_PATH_VARIABLE_NAME } from './constants';
 import { IPlatformService } from './types';
 
 @injectable()
@@ -24,7 +24,7 @@ export class PlatformService implements IPlatformService {
         }
     }
     public get pathVariableName() {
-        return this.isWindows ? WINDOWS_PATH_VARIABLE_NAME : NON_WINDOWS_PATH_VARIABLE_NAME;
+        return getSearchPathEnvVarNames(this.osType)[0];
     }
     public get virtualEnvBinName() {
         return this.isWindows ? 'Scripts' : 'bin';
@@ -72,8 +72,6 @@ export class PlatformService implements IPlatformService {
         return os.release();
     }
     public get is64bit(): boolean {
-        // tslint:disable-next-line:no-require-imports
-        const arch = require('arch');
-        return arch() === 'x64';
+        return getArchitecture() === Architecture.x64;
     }
 }

src/client/common/utils/async.ts
@@ -114,12 +114,16 @@ export function createDeferredFromPromise<T>(promise: Promise<T>): Deferred<T> {
 //================================
 // iterators
 
+export interface IAsyncIterator<T> extends AsyncIterator<T, void>, Partial<AsyncIterable<T>> {}
+
+export interface IAsyncIterableIterator<T> extends IAsyncIterator<T>, AsyncIterable<T> {}
+
 /**
  * An iterator that yields nothing.
  */
-export function iterEmpty<T>(): AsyncIterator<T, void> {
+export function iterEmpty<T>(): IAsyncIterableIterator<T> {
     // tslint:disable-next-line:no-empty
-    return ((async function* () {})() as unknown) as AsyncIterator<T, void>;
+    return ((async function* () {})() as unknown) as IAsyncIterableIterator<T>;
 }
 
 type NextResult<T> = { index: number } & (
@@ -153,7 +157,7 @@ export async function* chain<T>(
     iterators: AsyncIterator<T, T | void>[],
     onError?: (err: Error, index: number) => Promise<void>
     // Ultimately we may also want to support cancellation.
-): AsyncIterator<T, void> {
+): IAsyncIterableIterator<T> {
     const promises = iterators.map(getNext);
     let numRunning = iterators.length;
     while (numRunning > 0) {
@@ -193,7 +197,7 @@ export async function* mapToIterator<T, R = T>(
     items: T[],
     func: (item: T) => Promise<R>,
     race = true
-): AsyncIterator<R, void> {
+): IAsyncIterableIterator<R> {
     if (race) {
         const iterators = items.map((item) => {
             async function* generator() {
@@ -210,8 +214,8 @@ export async function* mapToIterator<T, R = T>(
 /**
  * Convert an iterator into an iterable, if it isn't one already.
  */
-export function iterable<T>(iterator: AsyncIterator<T, void>): AsyncIterableIterator<T> {
-    const it = iterator as AsyncIterableIterator<T>;
+export function iterable<T>(iterator: IAsyncIterator<T>): IAsyncIterableIterator<T> {
+    const it = iterator as IAsyncIterableIterator<T>;
     if (it[Symbol.asyncIterator] === undefined) {
         it[Symbol.asyncIterator] = () => it;
     }
@@ -221,14 +225,10 @@ export function iterable<T>(iterator: AsyncIterator<T, void>): AsyncIterableIter
 /**
  * Get everything yielded by the iterator.
  */
-export async function flattenIterator<T>(iterator: AsyncIterator<T, void>): Promise<T[]> {
+export async function flattenIterator<T>(iterator: IAsyncIterator<T>): Promise<T[]> {
     const results: T[] = [];
-    // We are dealing with an iterator, not an iterable, so we have
-    // to iterate manually rather than with a for-await loop.
-    let result = await iterator.next();
-    while (!result.done) {
-        results.push(result.value);
-        result = await iterator.next();
+    for await (const item of iterable(iterator)) {
+        results.push(item);
     }
     return results;
 }

src/client/common/utils/exec.ts
@@ -1,0 +1,55 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import * as fsapi from 'fs';
+import * as path from 'path';
+import { getEnvironmentVariable, getOSType, OSType } from './platform';
+
+/**
+ * Determine the env var to use for the executable search path.
+ */
+export function getSearchPathEnvVarNames(ostype = getOSType()): ('Path' | 'PATH')[] {
+    if (ostype === OSType.Windows) {
+        // On Windows both are supported now.
+        return ['Path', 'PATH'];
+    }
+    return ['PATH'];
+}
+
+/**
+ * Get the OS executable lookup "path" from the appropriate env var.
+ */
+export function getSearchPathEntries(): string[] {
+    const envVars = getSearchPathEnvVarNames();
+    for (const envVar of envVars) {
+        const value = getEnvironmentVariable(envVar);
+        if (value !== undefined) {
+            return parseSearchPathEntries(value);
+        }
+    }
+    // No env var was set.
+    return [];
+}
+
+function parseSearchPathEntries(envVarValue: string): string[] {
+    return envVarValue
+        .split(path.delimiter)
+        .map((entry: string) => entry.trim())
+        .filter((entry) => entry.length > 0);
+}
+
+/**
+ * Determine if the given file is executable by the current user.
+ *
+ * If the file does not exist or has any other problem when accessed
+ * then `false` is returned.  The caller is responsible to determine
+ * whether or not the file exists.
+ */
+export async function isValidAndExecutable(filename: string): Promise<boolean | undefined> {
+    try {
+        await fsapi.promises.access(filename, fsapi.constants.X_OK);
+    } catch (err) {
+        return false;
+    }
+    return true;
+}

src/client/common/utils/filesystem.ts
@@ -1,0 +1,30 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import * as path from 'path';
+import { getOSType, OSType } from './platform';
+
+/**
+ * Produce a uniform representation of the given filename.
+ *
+ * The result is especially suitable for cases where a filename is used
+ * as a key (e.g. in a mapping).
+ */
+export function normalizeFilename(filename: string): string {
+    // `path.resolve()` returns the absolute path.  Note that it also
+    // has the same behavior as `path.normalize()`.
+    const resolved = path.resolve(filename);
+    return getOSType() === OSType.Windows ? resolved.toLowerCase() : resolved;
+}
+
+/**
+ * Decide if the two filenames are the same file.
+ *
+ * This only checks the filenames (after normalizing) and does not
+ * resolve symlinks or other indirection.
+ */
+export function areSameFilename(filename1: string, filename2: string): boolean {
+    const norm1 = normalizeFilename(filename1);
+    const norm2 = normalizeFilename(filename2);
+    return norm1 === norm2;
+}

src/client/pythonEnvironments/base/envsCache.ts
@@ -1,9 +1,81 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+// tslint:disable-next-line:no-single-line-block-comment
+/* eslint-disable max-classes-per-file */
+
 import { cloneDeep } from 'lodash';
 import { PythonEnvInfo } from './info';
-import { areSameEnv } from './info/env';
+import {
+    areSameEnv,
+    getEnvExecutable,
+    haveSameExecutables,
+} from './info/env';
+
+/**
+ * A simple in-memory store of Python envs.
+ */
+export class PythonEnvsCache {
+    private envs: PythonEnvInfo[] = [];
+
+    private byExecutable: Record<string, PythonEnvInfo> | undefined;
+
+    constructor(envs?: PythonEnvInfo[]) {
+        if (envs !== undefined) {
+            this.setEnvs(envs);
+        }
+    }
+
+    /**
+     * Provide a copy of the cached envs.
+     */
+    public getEnvs(): PythonEnvInfo[] {
+        return cloneDeep(this.envs);
+    }
+
+    /**
+     * Replace the set of cached envs with the given set.
+     *
+     * If the given envs are the same as the cached ones
+     * then the existing are left in place.
+     *
+     * @returns - `true` if the cached envs were actually replaced
+     */
+    public setEnvs(envs: PythonEnvInfo[]): boolean {
+        // We *could* compare additional properties, but checking
+        // the executables is good enough for now.
+        if (haveSameExecutables(this.envs, envs)) {
+            return false;
+        }
+        this.envs = cloneDeep(envs);
+        this.byExecutable = undefined;
+        return true;
+    }
+
+    /**
+     * Find the matching env in the cache, if any.
+     */
+    public lookUp(
+        query: string | Partial<PythonEnvInfo>,
+    ): PythonEnvInfo | undefined {
+        const executable = getEnvExecutable(query);
+        if (executable === '') {
+            return undefined;
+        }
+        if (this.byExecutable === undefined) {
+            this.byExecutable = {};
+            for (const env of this.envs) {
+                this.byExecutable[env.executable.filename] = env;
+            }
+        }
+        return this.byExecutable[executable];
+    }
+
+    public filter(match: (env: PythonEnvInfo) => boolean | undefined): PythonEnvInfo[] {
+        const matched = this.envs.filter(match);
+        return cloneDeep(matched);
+    }
+}
 
 /**
  * Represents the environment info cache to be used by the cache locator.
@@ -60,7 +132,7 @@ type CompleteEnvInfoFunction = (envInfo: PythonEnvInfo) => boolean;
 export class PythonEnvInfoCache implements IEnvsCache {
     private initialized = false;
 
-    private envsList: PythonEnvInfo[] | undefined;
+    private inMemory: PythonEnvsCache | undefined;
 
     private persistentStorage: IPersistentStorage | undefined;
 
@@ -77,28 +149,27 @@ export class PythonEnvInfoCache implements IEnvsCache {
         this.initialized = true;
         if (this.getPersistentStorage !== undefined) {
             this.persistentStorage = this.getPersistentStorage();
-            this.envsList = await this.persistentStorage.load();
+            const envs = await this.persistentStorage.load();
+            if (envs !== undefined) {
+                this.setAllEnvs(envs);
+            }
         }
     }
 
     public getAllEnvs(): PythonEnvInfo[] | undefined {
-        return cloneDeep(this.envsList);
+        return this.inMemory?.getEnvs();
     }
 
     public setAllEnvs(envs: PythonEnvInfo[]): void {
-        this.envsList = cloneDeep(envs);
+        this.inMemory = new PythonEnvsCache(envs);
     }
 
     public filterEnvs(query: Partial<PythonEnvInfo>): PythonEnvInfo[] | undefined {
-        if (this.envsList === undefined) {
-            return undefined;
-        }
-        const result = this.envsList.filter((info) => areSameEnv(info, query));
-        return cloneDeep(result);
+        return this.inMemory?.filter((info) => areSameEnv(info, query));
     }
 
     public async flush(): Promise<void> {
-        const completeEnvs = this.envsList?.filter(this.isComplete);
+        const completeEnvs = this.inMemory?.filter(this.isComplete);
 
         if (completeEnvs?.length) {
             await this.persistentStorage?.store(completeEnvs);

commit 497c6f9413ed4daec5be7bab3243e0ed9feac671

Fix the PlatformService function test "is64bit. (#14677)
The test was broken on Windows by PR #14326.

src/client/common/utils/platform.ts
@@ -40,7 +40,13 @@ const architectures: Record<string, Architecture> = {
  * Identify the host's native architecture/bitness.
  */
 export function getArchitecture(): Architecture {
-    return architectures[process.arch] || Architecture.Unknown;
+    const fromProc = architectures[process.arch];
+    if (fromProc !== undefined) {
+        return fromProc;
+    }
+    // tslint:disable-next-line:no-require-imports
+    const arch = require('arch');
+    return architectures[arch()] || Architecture.Unknown;
 }
 
 /**