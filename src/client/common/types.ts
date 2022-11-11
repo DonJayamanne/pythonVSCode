f671b9bb7f932d465b2b9a7891185d89bf961ed9
Kartik Raj <karraj@microsoft.com>
2022-11-11 02:19:49 -0800

    Ensure we don't use the same interpreter for equally named project on different hosts (#20212)
    Closes https://github.com/microsoft/vscode-python/issues/19859
    
    Include remote name in path keys for fetch interpreter setting value

--- src/client/common/types.ts
+++ src/client/common/types.ts
@@ -479,6 +479,7 @@ export interface IInterpreterPathService {
     get(resource: Resource): string;
     inspect(resource: Resource): InspectInterpreterSettingType;
     update(resource: Resource, configTarget: ConfigurationTarget, value: string | undefined): Promise<void>;
+    copyOldInterpreterStorageValuesToNew(resource: Resource): Promise<void>;
 }
 
 export type DefaultLSType = LanguageServerType.Jedi | LanguageServerType.Node;
