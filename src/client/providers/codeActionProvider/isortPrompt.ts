cd6ca9d76f0db98cdcde750390ccf4631090b1f8
Karthik Nadig <kanadig@microsoft.com>
2023-02-06 15:56:21 -0800

    Remove `isort` extension dependency (#20577)
    Closes https://github.com/microsoft/vscode-python/issues/20586

--- src/client/providers/codeActionProvider/isortPrompt.ts
+++ src/client/providers/codeActionProvider/isortPrompt.ts
@@ -2,74 +2,77 @@
 // Licensed under the MIT License.
 
 import { IApplicationEnvironment } from '../../common/application/types';
+import { IPersistentState, IPersistentStateFactory } from '../../common/types';
 import { Common, ToolsExtensions } from '../../common/utils/localize';
 import { executeCommand } from '../../common/vscodeApis/commandApis';
+import { isExtensionDisabled, isExtensionEnabled } from '../../common/vscodeApis/extensionsApi';
 import { showInformationMessage } from '../../common/vscodeApis/windowApis';
 import { IServiceContainer } from '../../ioc/types';
 import { sendTelemetryEvent } from '../../telemetry';
 import { EventName } from '../../telemetry/constants';
-import { doNotShowPromptState, inToolsExtensionsExperiment, isExtensionDisabled, isExtensionEnabled } from './common';
-import { IToolsExtensionPrompt } from './types';
 
-export const PYLINT_EXTENSION = 'ms-python.pylint';
-const PYLINT_PROMPT_DONOTSHOW_KEY = 'showPylintExtensionPrompt';
+export const ISORT_EXTENSION = 'ms-python.isort';
+const ISORT_PROMPT_DONOTSHOW_KEY = 'showISortExtensionPrompt';
 
-export class PylintExtensionPrompt implements IToolsExtensionPrompt {
+function doNotShowPromptState(serviceContainer: IServiceContainer, promptKey: string): IPersistentState<boolean> {
+    const persistFactory: IPersistentStateFactory = serviceContainer.get<IPersistentStateFactory>(
+        IPersistentStateFactory,
+    );
+    return persistFactory.createWorkspacePersistentState<boolean>(promptKey, false);
+}
+
+export class ISortExtensionPrompt {
     private shownThisSession = false;
 
     public constructor(private readonly serviceContainer: IServiceContainer) {}
 
     public async showPrompt(): Promise<boolean> {
-        const isEnabled = isExtensionEnabled(this.serviceContainer, PYLINT_EXTENSION);
-        if (isEnabled || isExtensionDisabled(this.serviceContainer, PYLINT_EXTENSION)) {
+        const isEnabled = isExtensionEnabled(ISORT_EXTENSION);
+        if (isEnabled || isExtensionDisabled(ISORT_EXTENSION)) {
             sendTelemetryEvent(EventName.TOOLS_EXTENSIONS_ALREADY_INSTALLED, undefined, {
-                extensionId: PYLINT_EXTENSION,
+                extensionId: ISORT_EXTENSION,
                 isEnabled,
             });
             return true;
         }
 
-        const doNotShow = doNotShowPromptState(this.serviceContainer, PYLINT_PROMPT_DONOTSHOW_KEY);
+        const doNotShow = doNotShowPromptState(this.serviceContainer, ISORT_PROMPT_DONOTSHOW_KEY);
         if (this.shownThisSession || doNotShow.value) {
             return false;
         }
 
-        if (!(await inToolsExtensionsExperiment(this.serviceContainer))) {
-            return false;
-        }
-
-        sendTelemetryEvent(EventName.TOOLS_EXTENSIONS_PROMPT_SHOWN, undefined, { extensionId: PYLINT_EXTENSION });
+        sendTelemetryEvent(EventName.TOOLS_EXTENSIONS_PROMPT_SHOWN, undefined, { extensionId: ISORT_EXTENSION });
         this.shownThisSession = true;
         const response = await showInformationMessage(
-            ToolsExtensions.pylintPromptMessage,
-            ToolsExtensions.installPylintExtension,
+            ToolsExtensions.isortPromptMessage,
+            ToolsExtensions.installISortExtension,
             Common.doNotShowAgain,
         );
 
         if (response === Common.doNotShowAgain) {
             await doNotShow.updateValue(true);
             sendTelemetryEvent(EventName.TOOLS_EXTENSIONS_PROMPT_DISMISSED, undefined, {
-                extensionId: PYLINT_EXTENSION,
+                extensionId: ISORT_EXTENSION,
                 dismissType: 'doNotShow',
             });
             return false;
         }
 
-        if (response === ToolsExtensions.installPylintExtension) {
+        if (response === ToolsExtensions.installISortExtension) {
             sendTelemetryEvent(EventName.TOOLS_EXTENSIONS_INSTALL_SELECTED, undefined, {
-                extensionId: PYLINT_EXTENSION,
+                extensionId: ISORT_EXTENSION,
             });
             const appEnv: IApplicationEnvironment = this.serviceContainer.get<IApplicationEnvironment>(
                 IApplicationEnvironment,
             );
-            await executeCommand('workbench.extensions.installExtension', PYLINT_EXTENSION, {
+            await executeCommand('workbench.extensions.installExtension', ISORT_EXTENSION, {
                 installPreReleaseVersion: appEnv.extensionChannel === 'insiders',
             });
             return true;
         }
 
         sendTelemetryEvent(EventName.TOOLS_EXTENSIONS_PROMPT_DISMISSED, undefined, {
-            extensionId: PYLINT_EXTENSION,
+            extensionId: ISORT_EXTENSION,
             dismissType: 'close',
         });
 
@@ -77,10 +80,10 @@ export class PylintExtensionPrompt implements IToolsExtensionPrompt {
     }
 }
 
-let _prompt: IToolsExtensionPrompt | undefined;
-export function getOrCreatePylintPrompt(serviceContainer: IServiceContainer): IToolsExtensionPrompt {
+let _prompt: ISortExtensionPrompt | undefined;
+export function getOrCreateISortPrompt(serviceContainer: IServiceContainer): ISortExtensionPrompt {
     if (!_prompt) {
-        _prompt = new PylintExtensionPrompt(serviceContainer);
+        _prompt = new ISortExtensionPrompt(serviceContainer);
     }
     return _prompt;
 }
