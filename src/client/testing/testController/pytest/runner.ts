e0558ce5e73240f10de4d1b8105cefe381f682e5
Karthik Nadig <kanadig@microsoft.com>
2023-05-01 09:08:44 -0700

    Revert "Default to `--no-cov` when debugging pytest" (#21152)
    Reverts microsoft/vscode-python#21048
    
    Closes https://github.com/microsoft/vscode-python/issues/21146
    Related https://github.com/microsoft/vscode-python/issues/19985

--- src/client/testing/testController/pytest/runner.ts
+++ src/client/testing/testController/pytest/runner.ts
@@ -95,10 +95,6 @@ export class PytestRunner implements ITestsRunner {
                 testArgs.push('--capture', 'no');
             }
 
-            if (options.debug && !testArgs.some((a) => a.startsWith('--no-cov'))) {
-                testArgs.push('--no-cov');
-            }
-
             // Positional arguments control the tests to be run.
             const rawData = idToRawData.get(testNode.id);
             if (!rawData) {
