e07aa5ed30e0f54e5b1ee718ee6ef409db4dc946
Karthik Nadig <kanadig@microsoft.com>
2023-07-13 18:20:35 -0700

    Update writing style based on recommendation (#21525)
    Closes https://github.com/microsoft/vscode-python/issues/21521
    Closes https://github.com/microsoft/vscode-python/issues/21519
    
    ---------
    
    Co-authored-by: Brett Cannon <brett@python.org>
    Co-authored-by: Courtney Webster <60238438+cwebster-99@users.noreply.github.com>
    Co-authored-by: Luciana Abud <45497113+luabud@users.noreply.github.com>

--- src/client/debugger/extension/adapter/factory.ts
+++ src/client/debugger/extension/adapter/factory.ts
@@ -203,6 +203,6 @@ export class DebugAdapterDescriptorFactory implements IDebugAdapterDescriptorFac
      * @memberof DebugAdapterDescriptorFactory
      */
     private async notifySelectInterpreter() {
-        await showErrorMessage(l10n.t('Please install Python or select a Python Interpreter to use the debugger.'));
+        await showErrorMessage(l10n.t('Install Python or select a Python Interpreter to use the debugger.'));
     }
 }
