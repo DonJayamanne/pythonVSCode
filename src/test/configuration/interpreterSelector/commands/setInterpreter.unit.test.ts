2f3102fe0bb007df0d80276f488c2d0257f4f3b1
Karthik Nadig <kanadig@microsoft.com>
2024-01-16 11:10:38 -0800

    Add create environment to select interpreter (#22746)
    Closes https://github.com/microsoft/vscode-python/issues/22664
    
    
    ![image](https://github.com/microsoft/vscode-python/assets/3840081/918908ec-e9fa-4dd5-b274-092b5a129971)

--- src/test/configuration/interpreterSelector/commands/setInterpreter.unit.test.ts
+++ src/test/configuration/interpreterSelector/commands/setInterpreter.unit.test.ts
@@ -155,7 +155,11 @@ suite('Set Interpreter Command', () => {
             } as PythonEnvironment,
         };
         const expectedEnterInterpreterPathSuggestion = {
-            label: `${Octicons.Add} ${InterpreterQuickPickList.enterPath.label}`,
+            label: `${Octicons.Folder} ${InterpreterQuickPickList.enterPath.label}`,
+            alwaysShow: true,
+        };
+        const expectedCreateEnvSuggestion = {
+            label: `${Octicons.Add} ${InterpreterQuickPickList.create.label}`,
             alwaysShow: true,
         };
         const currentPythonPath = 'python';
@@ -237,6 +241,7 @@ suite('Set Interpreter Command', () => {
             recommended.description = interpreterPath;
             const suggestions = [
                 expectedEnterInterpreterPathSuggestion,
+                { kind: QuickPickItemKind.Separator, label: '' },
                 defaultInterpreterPathSuggestion,
                 { kind: QuickPickItemKind.Separator, label: EnvGroups.Recommended },
                 recommended,
@@ -278,11 +283,66 @@ suite('Set Interpreter Command', () => {
             assert.deepStrictEqual(actualParameters, expectedParameters, 'Params not equal');
         });
 
+        test('Picker should show create env when set in options', async () => {
+            const state: InterpreterStateArgs = { path: 'some path', workspace: undefined };
+            const multiStepInput = TypeMoq.Mock.ofType<IMultiStepInput<InterpreterStateArgs>>();
+            const recommended = cloneDeep(item);
+            recommended.label = `${Octicons.Star} ${item.label}`;
+            recommended.description = interpreterPath;
+            const suggestions = [
+                expectedCreateEnvSuggestion,
+                { kind: QuickPickItemKind.Separator, label: '' },
+                expectedEnterInterpreterPathSuggestion,
+                { kind: QuickPickItemKind.Separator, label: '' },
+                defaultInterpreterPathSuggestion,
+                { kind: QuickPickItemKind.Separator, label: EnvGroups.Recommended },
+                recommended,
+            ];
+            const expectedParameters: IQuickPickParameters<QuickPickItem> = {
+                placeholder: `Selected Interpreter: ${currentPythonPath}`,
+                items: suggestions,
+                matchOnDetail: true,
+                matchOnDescription: true,
+                title: InterpreterQuickPickList.browsePath.openButtonLabel,
+                sortByLabel: true,
+                keepScrollPosition: true,
+            };
+            let actualParameters: IQuickPickParameters<QuickPickItem> | undefined;
+            multiStepInput
+                .setup((i) => i.showQuickPick(TypeMoq.It.isAny()))
+                .callback((options) => {
+                    actualParameters = options;
+                })
+                .returns(() => Promise.resolve((undefined as unknown) as QuickPickItem));
+
+            await setInterpreterCommand._pickInterpreter(multiStepInput.object, state, undefined, {
+                showCreateEnvironment: true,
+            });
+
+            expect(actualParameters).to.not.equal(undefined, 'Parameters not set');
+            const refreshButtons = actualParameters!.customButtonSetups;
+            expect(refreshButtons).to.not.equal(undefined, 'Callback not set');
+            delete actualParameters!.initialize;
+            delete actualParameters!.customButtonSetups;
+            delete actualParameters!.onChangeItem;
+            if (typeof actualParameters!.activeItem === 'function') {
+                const activeItem = await actualParameters!.activeItem(({ items: suggestions } as unknown) as QuickPick<
+                    QuickPickType
+                >);
+                assert.deepStrictEqual(activeItem, recommended);
+            } else {
+                assert(false, 'Not a function');
+            }
+            delete actualParameters!.activeItem;
+            assert.deepStrictEqual(actualParameters, expectedParameters, 'Params not equal');
+        });
+
         test('Picker should be displayed with expected items if no interpreters are available', async () => {
             const state: InterpreterStateArgs = { path: 'some path', workspace: undefined };
             const multiStepInput = TypeMoq.Mock.ofType<IMultiStepInput<InterpreterStateArgs>>();
             const suggestions = [
                 expectedEnterInterpreterPathSuggestion,
+                { kind: QuickPickItemKind.Separator, label: '' },
                 defaultInterpreterPathSuggestion,
                 noPythonInstalled,
             ];
@@ -440,6 +500,7 @@ suite('Set Interpreter Command', () => {
             recommended.description = interpreterPath;
             const suggestions = [
                 expectedEnterInterpreterPathSuggestion,
+                { kind: QuickPickItemKind.Separator, label: '' },
                 defaultInterpreterPathSuggestion,
                 { kind: QuickPickItemKind.Separator, label: EnvGroups.Recommended },
                 recommended,
@@ -556,6 +617,7 @@ suite('Set Interpreter Command', () => {
             recommended.description = interpreterPath;
             const suggestions = [
                 expectedEnterInterpreterPathSuggestion,
+                { kind: QuickPickItemKind.Separator, label: '' },
                 defaultInterpreterPathSuggestion,
                 { kind: QuickPickItemKind.Separator, label: EnvGroups.Recommended },
                 recommended,
@@ -652,7 +714,13 @@ suite('Set Interpreter Command', () => {
                 alwaysShow: true,
             };
 
-            const suggestions = [expectedEnterInterpreterPathSuggestion, defaultPathSuggestion, separator, recommended];
+            const suggestions = [
+                expectedEnterInterpreterPathSuggestion,
+                { kind: QuickPickItemKind.Separator, label: '' },
+                defaultPathSuggestion,
+                separator,
+                recommended,
+            ];
             const expectedParameters: IQuickPickParameters<QuickPickItem> = {
                 placeholder: `Selected Interpreter: ${currentPythonPath}`,
                 items: suggestions,
