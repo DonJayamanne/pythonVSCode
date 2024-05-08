204309e45e652fa4c8ca5be624cd2b464cf4fd16
Don Jayamanne <don.jayamanne@outlook.com>
2024-05-08 10:14:24 +1000

    Remove unwanted test feature (#23371)

--- native_locator/src/utils.rs
+++ native_locator/src/utils.rs
@@ -6,7 +6,7 @@ use std::{
     process::Command,
 };
 
-fn get_version_impl(path: &str) -> Option<String> {
+pub fn get_version(path: &str) -> Option<String> {
     let output = Command::new(path)
         .arg("-c")
         .arg("import sys; print(sys.version)")
@@ -16,24 +16,6 @@ fn get_version_impl(path: &str) -> Option<String> {
     let output = output.trim();
     let output = output.split_whitespace().next().unwrap_or(output);
     Some(output.to_string())
-}
-
-#[cfg(not(feature = "test"))]
-pub fn get_version(path: &str) -> Option<String> {
-    get_version_impl(path)
-}
-
-// Tests
-
-#[cfg(feature = "test")]
-pub fn get_version(path: &str) -> Option<String> {
-    use std::path::PathBuf;
-    let version_file = PathBuf::from(path.to_owned() + ".version");
-    if version_file.exists() {
-        let version = std::fs::read_to_string(version_file).ok()?;
-        return Some(version.trim().to_string());
-    }
-    get_version_impl(path)
 }
 
 pub fn find_python_binary_path(env_path: &Path) -> Option<PathBuf> {
