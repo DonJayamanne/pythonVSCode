daebb5cb77e36e683a4bf29fca10b6ebf62922c1
Karthik Nadig <kanadig@microsoft.com>
2024-06-20 22:20:04 -0700

    Add support for Python Environment Tools  (#23643)
    Closes https://github.com/microsoft/vscode-python/issues/23564
    
    ---------
    
    Co-authored-by: Raymond Zhao <7199958+rzhao271@users.noreply.github.com>

--- noxfile.py
+++ noxfile.py
@@ -6,6 +6,7 @@ import pathlib
 import nox
 import shutil
 import sysconfig
+import uuid
 
 EXT_ROOT = pathlib.Path(__file__).parent
 
@@ -48,13 +49,47 @@ def install_python_libs(session: nox.Session):
 
 
 @nox.session()
-def native_build(session: nox.Session):
-    with session.cd("./native_locator"):
-        if not pathlib.Path(pathlib.Path.cwd() / "bin").exists():
-            pathlib.Path(pathlib.Path.cwd() / "bin").mkdir()
+def azure_pet_build_before(session: nox.Session):
+    source_dir = pathlib.Path(pathlib.Path.cwd() / "python-env-tools").resolve()
+    config_toml_disabled = source_dir / ".cargo" / "config.toml.disabled"
+    config_toml = source_dir / ".cargo" / "config.toml"
+    if config_toml_disabled.exists() and not config_toml.exists():
+        config_toml.write_bytes(config_toml_disabled.read_bytes())
 
-        if not pathlib.Path(pathlib.Path.cwd() / "bin" / ".gitignore").exists():
-            pathlib.Path(pathlib.Path.cwd() / "bin" / ".gitignore").write_text(
+
+@nox.session()
+def azure_pet_build_after(session: nox.Session):
+    source_dir = pathlib.Path(pathlib.Path.cwd() / "python-env-tools").resolve()
+    ext = sysconfig.get_config_var("EXE") or ""
+    bin_name = f"pet{ext}"
+
+    abs_bin_path = None
+    for root, _, files in os.walk(os.fspath(source_dir / "target")):
+        bin_path = pathlib.Path(root) / "release" / bin_name
+        if bin_path.exists():
+            abs_bin_path = bin_path.absolute()
+            break
+
+    assert abs_bin_path
+
+    dest_dir = pathlib.Path(pathlib.Path.cwd() / "python-env-tools").resolve()
+    if not pathlib.Path(dest_dir / "bin").exists():
+        pathlib.Path(dest_dir / "bin").mkdir()
+    bin_dest = dest_dir / "bin" / bin_name
+    shutil.copyfile(abs_bin_path, bin_dest)
+
+
+@nox.session()
+def native_build(session: nox.Session):
+    source_dir = pathlib.Path(pathlib.Path.cwd() / "python-env-tools").resolve()
+    dest_dir = pathlib.Path(pathlib.Path.cwd() / "python-env-tools").resolve()
+
+    with session.cd(source_dir):
+        if not pathlib.Path(dest_dir / "bin").exists():
+            pathlib.Path(dest_dir / "bin").mkdir()
+
+        if not pathlib.Path(dest_dir / "bin" / ".gitignore").exists():
+            pathlib.Path(dest_dir / "bin" / ".gitignore").write_text(
                 "*\n", encoding="utf-8"
             )
 
@@ -70,37 +105,88 @@ def native_build(session: nox.Session):
                 "--release",
                 "--target",
                 target,
-                "--package",
-                "python-finder",
                 external=True,
             )
-            source = f"./target/{target}/release/python-finder{ext}"
-            dest = f"./bin/python-finder{ext}"
-            shutil.copy(source, dest)
+            source = source_dir / "target" / target / "release" / f"pet{ext}"
         else:
             session.run(
                 "cargo",
                 "build",
                 "--frozen",
                 "--release",
-                "--package",
-                "python-finder",
                 external=True,
             )
+            source = source_dir / "target" / "release" / f"pet{ext}"
+        dest = dest_dir / "bin" / f"pet{ext}"
+        shutil.copy(source, dest)
 
-            source = f"./target/release/python-finder{ext}"
-            dest = f"./bin/python-finder{ext}"
-            shutil.copy(source, dest)
-
-    # Remove native_locator/bin exclusion from .vscodeignore
+    # Remove python-env-tools/bin exclusion from .vscodeignore
     vscode_ignore = EXT_ROOT / ".vscodeignore"
-    remove_patterns = ("native_locator/bin/**",)
+    remove_patterns = ("python-env-tools/bin/**",)
     lines = vscode_ignore.read_text(encoding="utf-8").splitlines()
     filtered_lines = [line for line in lines if not line.startswith(remove_patterns)]
     vscode_ignore.write_text("\n".join(filtered_lines) + "\n", encoding="utf-8")
 
 
+def delete_dir(path: pathlib.Path, ignore_errors=None):
+    attempt = 0
+    known = []
+    while attempt < 5:
+        try:
+            shutil.rmtree(os.fspath(path), ignore_errors=ignore_errors)
+            return
+        except PermissionError as pe:
+            if os.fspath(pe.filename) in known:
+                break
+            print(f"Changing permissions on {pe.filename}")
+            os.chmod(pe.filename, 0o666)
+
+    shutil.rmtree(os.fspath(path))
+
+
+@nox.session()
+def checkout_native(session: nox.Session):
+    dest = (pathlib.Path.cwd() / "python-env-tools").resolve()
+    if dest.exists():
+        shutil.rmtree(os.fspath(dest))
+
+    tempdir = os.getenv("TEMP") or os.getenv("TMP") or "/tmp"
+    tempdir = pathlib.Path(tempdir) / str(uuid.uuid4()) / "python-env-tools"
+    tempdir.mkdir(0o666, parents=True)
+
+    session.log(f"Temp dir: {tempdir}")
+
+    session.log(f"Cloning python-environment-tools to {tempdir}")
+    try:
+        with session.cd(tempdir):
+            session.run("git", "init", external=True)
+            session.run(
+                "git",
+                "remote",
+                "add",
+                "origin",
+                "https://github.com/microsoft/python-environment-tools",
+                external=True,
+            )
+            session.run("git", "fetch", "origin", "main", external=True)
+            session.run(
+                "git", "checkout", "--force", "-B", "main", "origin/main", external=True
+            )
+            delete_dir(tempdir / ".git")
+            delete_dir(tempdir / ".github")
+            delete_dir(tempdir / ".vscode")
+            (tempdir / "CODE_OF_CONDUCT.md").unlink()
+            shutil.move(os.fspath(tempdir), os.fspath(dest))
+    except PermissionError as e:
+        print(f"Permission error: {e}")
+        if not dest.exists():
+            raise
+    finally:
+        delete_dir(tempdir.parent, ignore_errors=True)
+
+
 @nox.session()
 def setup_repo(session: nox.Session):
     install_python_libs(session)
+    checkout_native(session)
     native_build(session)
